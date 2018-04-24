f3d92a86668292f385819474a9b0e9715b09a353
Ignatovich Mikhail <m.s.ignat@gmail.com>
2018-04-24 08:49:11 +0300

    Logic to view reports

---
+++ admin/src/components/Report.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .container
+    h2 Email
+    input(v-model='form.logemail')
+    h2 Password
+    input(v-model='form.logpassword')
+    button(@click='sendForm') Log In
+    h2(v-if='isLogin') You are logged in!
+
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        logemail: "",
+        logpassword: ""
+      },
+      isLogin: false,      
+    };
+  },
+  methods: {
+  },
+  computed: {},
+  mounted() {},
+  components: {}
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+</style>
