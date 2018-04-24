7a0676ed28566bee53b8b9b3cf9b6a91cbedcc1f
Valerii Voronkov <voronglass84@gmail.com>
2018-04-24 19:00:46 +0300

    Added login form with adaptive

--- client/pages/Login.vue
+++ client/pages/Login.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
-  .container
-    h2 Email
-    input(v-model='form.logemail')
-    h2 Password
-    input(v-model='form.logpassword')
-    button(@click='sendForm') Log In
-    h2(v-if='isLogin') You are logged in!
+  .loginWrapper
+    .h2Wrapper
+      h2 Email
+      input(v-model='form.logemail')
+    .h2Wrapper
+      h2 Password
+      input(v-model='form.logpassword')
+    .buttonWrapper
+      button(@click='sendForm') Log In
+      h2(v-if='isLogin') You are logged in!
 
 </template>
 <script>
@@ -43,5 +46,47 @@ export default {
 </script>
 
 <style lang="scss">
+  body {
+    margin: 0;
+    overflow-y: hidden;
+  }
 
+  .loginWrapper {
+    background-color: #bfb3b3;
+    margin: 20% auto;
+    width: 350px;
+    @media (max-width: 374px) {
+      width: 300px;
+    }
+
+    border-radius: 26px;
+
+    .h2Wrapper {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    h2 {
+      color: #fff;
+      margin: 3%;
+    }
+
+    input {
+      height: 26px;
+      width: 70%;
+    }
+
+    .buttonWrapper {
+      display: flex;
+      justify-content: center;
+      margin: 7% 0 1.2% 0;
+      padding-bottom: 5%;
+        button {
+          width: 37%;
+          height: 38px;
+          border-radius: 8px;
+        }
+    }
+  }
 </style>
