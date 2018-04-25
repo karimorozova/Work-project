2fc34089ba81eb04616e234c753ae8fd80d4f5ba
Valerii Voronkov <voronglass84@gmail.com>
2018-04-25 18:22:47 +0300

    Added part of login form

--- portal/pages/Login.vue
+++ portal/pages/Login.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
   .loginWrapper
+    .imageWrapper
+      img(src="../assets/images/login_logo.png")
     .h2Wrapper
       h2 Email
-      input(v-model='form.logemail')
+      input(v-model='form.logemail' placeholder='Email')
     .h2Wrapper
       h2 Password
-      input(type="password" v-model='form.logpassword')
+      input(type="password" v-model='form.logpassword' placeholder='Password')
     .buttonWrapper
       button(@click='sendForm') Log In
       h2(v-if='isLogin') You are logged in!
@@ -28,14 +30,9 @@ export default {
     sendForm() {
       this.$axios.post("/auth", this.form).then(
         response => {
-          var now = new Date();
-          var time = now.getTime();
-          var expireTime = time + 1000*60;
-          now.setTime(expireTime);
-
-          document.cookie =  "ses=" + response.data + ";max-age=60";
-          //console.log("ses=" + response.data + ";expires=" + now.toLocaleString());
-          console.log(now.toLocaleString());
+          let date = new Date();
+          date.setDate(date.getSeconds() + 30)
+          document.cookie =  "ses=" + response.data; + `;expires=${date}`;
           console.log(response);
           this.isLogin = true;
 
@@ -61,10 +58,11 @@ export default {
   body {
     margin: 0;
     overflow-y: hidden;
+    background-image: url('/assets/images/image-background.jpg');
   }
 
   .loginWrapper {
-    background-color: #bfb3b3;
+    background-color: #fff;
     margin: 20% auto;
     width: 603px;
     @media (max-width: 625px) {
@@ -79,6 +77,16 @@ export default {
 
     border-radius: 26px;
 
+    .imageWrapper {
+      position: absolute;
+      top: 24%;
+      left: 43%;
+      img {
+        width: 269px;
+        height: 76px;
+      }
+    }
+
     .h2Wrapper {
       display: flex;
       flex-direction: column;
@@ -91,8 +99,8 @@ export default {
     }
 
     input {
-      height: 26px;
-      width: 70%;
+      height: 41px;
+      width: 378px;
     }
 
     .buttonWrapper {
@@ -101,8 +109,8 @@ export default {
       margin: 7% 0 1.2% 0;
       padding-bottom: 5%;
         button {
-          width: 37%;
-          height: 38px;
+          width: 142px;
+          height: 35px;
           border-radius: 8px;
           font-size: 15px;
         }
