3ba15d26c6afdc43bf55b2c805a70fabb079e477
Michail Ignatovich <m.s.ignat@gmail.com>
2018-04-27 17:30:51 +0300

    cleared up models

--- admin/models/index.js
+++ admin/models/index.js
@@ -2,20 +2,13 @@ const Languages = require('./languages');
 const Requests = require('./requests');
 const User = require('./user');
 const Services = require('./services');
-const Customer = require('./customer');
 
-const { Xtrf, SmartProject } = require("./xtrf");
-const { ParseHTML } = require('./parser');
 
 const Models = {
     Languages,
     Requests,
     User,
-    Services,
-    Xtrf,
-    SmartProject,
-    ParseHTML,
-    Customer
+    Services
 };
 
 module.exports = Models;
