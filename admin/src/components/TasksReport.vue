4828c8cae7c22294cb23d0c149356542f561ca40
Michail Ignatovich <m.s.ignat@gmail.com>
2018-05-04 13:07:16 +0300

    added jobs to reports

--- admin/src/components/TasksReport.vue
+++ admin/src/components/TasksReport.vue
@@ -4,9 +4,33 @@
       h1 Summary
       table.table.reportTable(border='bordered')
         tr
-          th(v-for="title in titles") {{ title.header }}
+          th(v-for="title in titles") {{ title }}
         tr(v-for="res in summary")
-          td(v-for="column in res.columns") {{ column }}
+          td {{ res.projectId }}
+          td {{ res.projectName }}
+          td {{ res.beginDate }}
+          td {{ res.deadline }}
+          td {{ res.sourceLanguage }}
+          td {{ res.targetLanguage }}
+          td {{ res.projectService }}
+          td {{ res.providerName }}
+          td {{ res.jobService }}
+          td {{ res.providerRate }}
+          td {{ res.wordcount }}
+          td {{ res.wordcountRelative }}
+          td {{ res.totalCost }}
+          td {{ res.clientName }}
+          td {{ res.clientRate }}
+          td {{ res.wordcountReceivable }}
+          td {{ res.sumStep1 }}
+          td {{ res.sumStep2 }}          
+          td {{ res.sum }}
+          td {{ res.totalAgreed }}
+          td {{ res.porfit }}
+          td {{ res.porfitPerc }}
+          td {{ res.instructions }}
+          td {{ res.invoiced }}
+          td {{ res.jobId }}
           
         
 </template>
@@ -15,35 +39,34 @@ export default {
   data() {
     return {
       summary : [],
-      titles: []      
+      titles: ["Project ID", "Project Name", "Start Date and Time", "Project Deadline", "Source Language", "Target Language", "Project Service", 
+      "Provider Name", "Service", "Rate [Provider]", "Wordcount", "Relative Wordcount", "Total Cost", 
+      "Client Name", "Rate [Client]", "Wordcount [Receivable]", "Sum [Step1]", "Sum [Step2]", "Sum [Receivable]", "Total Agreed", 
+      "Profit", "Profit in %", "Internal Special Instructions", "Invoiced"
+      ]      
     };
   },
   methods: {
-    getQuotes() {
+    getReports() {
       console.log("Quotes are getting");
 
       this.$http.get("/reps").then(
         response => {
-          var month = JSON.parse(response.bodyText);
+          //var month = 
+          this.summary = JSON.parse(response.bodyText);
 
-          //console.log(JSON.parse(response.bodyText)[0]._id)
-          //this.orders = JSON.parse(response.bodyText);
-          
-          this.titles = month[0].columns;
-          this.summary = month[0].tasks;
-
-          console.log(month);
+          console.log(response.bodyText);
         },
         err => {
           console.log(`You have to log in ${err}`);
-          this.$router.push('/login')
+          //this.$router.push('/login')
         }
       );
     }
   },
   computed: {},
   created() {
-    this.getQuotes();
+    this.getReports();
   },
   components: {}
 };
