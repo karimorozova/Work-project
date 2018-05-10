1f6fe84c1984fad3caee7fb9fd7decead1ed2cb8
Valerii Voronkov <voronglass84@gmail.com>
2018-05-10 09:39:52 +0300

    Start on the correction  Quote Details

--- portal/components/quotes/QuotesInfoDetailed.vue
+++ portal/components/quotes/QuotesInfoDetailed.vue
@@ -2,29 +2,29 @@
     .qoutesInfoDetailedWrapper
         .container
             .container__label
-                .container__label-title QuotesInfo
+                .container__label-title Quote Details
             .container__info
-                .container__info-first
-                    .projid
-                        .projid-name {{ quotesInfoDetailed.title }}
-                        .projid-value {{ quotesInfoDetailed.createdDate }}
-                    .requsted
-                        .requsted-name {{ quotesInfoDetailed.requested }}
-                        .requsted-value {{ quotesInfoDetailed.requstedDate }}
-                    .deadline
-                        .deadline-name {{ quotesInfoDetailed.deadline }}
-                        .deadline-value {{ quotesInfoDetailed.deadlineDate }}
-                .container__info-second
-                    .projectName
-                        .projectName-name {{ quotesInfoDetailed.projectName }}
-                        .projectName-value {{ quotesInfoDetailed.projectNameValue }}
-                    .status
-                        .status-name {{ quotesInfoDetailed.status }}
-                        .status-value {{ quotesInfoDetailed.statusDescription }}
-                    .totalcost
-                        .totalcost-name {{ quotesInfoDetailed.totalCost }}
-                        .totalcost-value(v-html="quotesInfoDetailed.totalCostValue")
-            .container-table
+              .container__info-first
+                  .projid
+                      .projid-name {{ quotesInfoDetailed.title }}
+                      .projid-value {{ quotesInfoDetailed.createdDate }}
+                  .requsted
+                      .requsted-name {{ quotesInfoDetailed.requested }}
+                      .requsted-value {{ quotesInfoDetailed.requstedDate }}
+                  .deadline
+                      .deadline-name {{ quotesInfoDetailed.deadline }}
+                      .deadline-value {{ quotesInfoDetailed.deadlineDate }}
+              .container__info-second
+                  .projectName
+                      .projectName-name {{ quotesInfoDetailed.projectName }}
+                      .projectName-value {{ quotesInfoDetailed.projectNameValue }}
+                  .status
+                      .status-name {{ quotesInfoDetailed.status }}
+                      .status-value {{ quotesInfoDetailed.statusDescription }}
+                  .totalcost
+                      .totalcost-name {{ quotesInfoDetailed.totalCost }}
+                      .totalcost-value(v-html="quotesInfoDetailed.totalCostValue")
+              .container-table
                 .container-table__header
                     .container-table__col-1
                       .firstPart ID
@@ -55,9 +55,12 @@
                         .container-table__col-2
                         .container-table__col-3 TOTAL COST:
                         .container-table__col-4 4
-            .container__buttons
-                button.approve APPROVE QUOTE
-                button.reject REJECT QUOTE
+              .container__buttons
+                  button.approve APPROVE QUOTE
+                  button.reject REJECT QUOTE
+            .project-manager
+              .project-manager__detailed_info Project Manager
+              .project-manager__detailed_description Services
 </template>
 
 <style lang="scss" scoped>
@@ -69,7 +72,7 @@
 
 .container {
   display: flex;
-  flex-direction: column;
+  // flex-direction: column;
   width: 100%;
 
   &__label {
@@ -81,6 +84,7 @@
       color: #67573e;
       border-bottom: 1px solid #c5bfb7;
       width: 100%;
+      font-size: 20px;
     }
   }
 
@@ -130,131 +134,134 @@
         }
       }
     }
-    border-bottom: 1px solid #c5bfb7;
-  }
 
-  .container-table {
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    margin-left: 2%;
-    margin-top: 2%;
-    color: #978d7e;
-
-    &__header {
+    .container-table {
       display: flex;
-      flex-direction: columns;
-      color: #67573e;
-    }
-
-    &__inner-ceils {
-      display: flex;
+      width: 100%;
       flex-direction: column;
-    }
+      margin-left: 2%;
+      margin-top: 2%;
+      color: #978d7e;
 
-    &__row {
-      display: flex;
-      &:last-child {
-        .container-table__col-1 {
-          border: none;
+      &__header {
+        display: flex;
+        flex-direction: columns;
+        color: #67573e;
+      }
+
+      &__inner-ceils {
+        display: flex;
+        flex-direction: column;
+      }
+
+      &__row {
+        display: flex;
+        &:last-child {
+          .container-table__col-1 {
+            border: none;
+          }
+
+          .container-table__col-2 {
+            border: none;
+          }
+
+          .container-table__col-3 {
+            border: none;
+            margin-right: 0.5%;
+          }
+        }
+      }
+
+      &__col-1 {
+        flex-basis: 25%;
+        border: 1px solid #978d7e;
+        padding: 5px 5px;
+        justify-content: flex-start;
+        display: flex;
+
+        .firstPart {
+          margin-left: 7%;
         }
 
-        .container-table__col-2 {
+        .secondPart {
+          margin-left: 6%;
+        }
+        &:last-child {
           border: none;
         }
+      }
 
-        .container-table__col-3 {
-          border: none;
-          margin-right: 0.5%;
+      &__col-2 {
+        flex-basis: 25%;
+        border: 1px solid #978d7e;
+        padding: 5px 5px;
+        justify-content: flex-start;
+        display: flex;
+
+        .firstPart {
+          margin-left: 7%;
+        }
+
+        .secondPart {
+          margin-left: 6%;
+        }
+      }
+
+      &__col-3 {
+        flex-basis: 25%;
+        border: 1px solid #978d7e;
+        padding: 5px 5px;
+        justify-content: flex-start;
+        display: flex;
+
+        .firstPart {
+          margin-left: 7%;
+        }
+
+        .secondPart {
+          margin-left: 6%;
+        }
+      }
+
+      &__col-4 {
+        flex-basis: 16%;
+        border: 1px solid #978d7e;
+        padding: 5px 5px;
+
+        .addition {
+          margin-left: 7%;
         }
       }
     }
 
-    &__col-1 {
-      flex-basis: 25%;
-      border: 1px solid #978d7e;
-      padding: 5px 5px;
-      justify-content: flex-start;
+    &__buttons {
       display: flex;
+      justify-content: flex-end;
 
-      .firstPart {
-        margin-left: 7%;
-      }
-
-      .secondPart {
-        margin-left: 6%;
-      }
-      &:last-child {
+      button {
+        width: 164px;
+        height: 40px;
+        color: #fff;
+        box-shadow: 0 5px 8px rgba(103, 87, 62, 0.5);
+        border-radius: 18px;
         border: none;
-      }
-    }
-
-    &__col-2 {
-      flex-basis: 25%;
-      border: 1px solid #978d7e;
-      padding: 5px 5px;
-      justify-content: flex-start;
-      display: flex;
-
-      .firstPart {
-        margin-left: 7%;
+        margin-right: 2%;
+        margin-left: 2%;
+        font-size: 15px;
       }
 
-      .secondPart {
-        margin-left: 6%;
-      }
-    }
-
-    &__col-3 {
-      flex-basis: 25%;
-      border: 1px solid #978d7e;
-      padding: 5px 5px;
-      justify-content: flex-start;
-      display: flex;
-
-      .firstPart {
-        margin-left: 7%;
+      .approve {
+        background-color: #84ca8e;
       }
 
-      .secondPart {
-        margin-left: 6%;
-      }
-    }
-
-    &__col-4 {
-      flex-basis: 16%;
-      border: 1px solid #978d7e;
-      padding: 5px 5px;
-
-      .addition {
-        margin-left: 7%;
+      .reject {
+        background-color: #f5876e;
       }
     }
   }
 
-  &__buttons {
-    display: flex;
-    justify-content: flex-end;
-
-    button {
-      width: 164px;
-      height: 40px;
-      color: #fff;
-      box-shadow: 0 5px 8px rgba(103, 87, 62, 0.5);
-      border-radius: 18px;
-      border: none;
-      margin-right: 2%;
-      margin-left: 2%;
-      font-size: 15px;
-    }
-
-    .approve {
-      background-color: #84ca8e;
-    }
-
-    .reject {
-      background-color: #f5876e;
-    }
+  .project-manager {
+    background-color: #f4f0ee;
   }
 }
 </style>
