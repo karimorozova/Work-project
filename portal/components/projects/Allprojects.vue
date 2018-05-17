a5d22315083191f5874024ed8f40ce3485f4a2b0
Daniyal <da-lu@lenta.ru>
2018-05-17 12:48:13 +0300

    fixed opening

--- portal/components/projects/Allprojects.vue
+++ portal/components/projects/Allprojects.vue
@@ -78,6 +78,9 @@ export default {
         }
     },
     methods: {
+    showAllProjects() {
+        this.openAll = !this.openAll
+    },
     sourceLangOpen() {
       this.openSourceLangs = !this.openSourceLangs;
     },
