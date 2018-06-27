c1fe773551df8394abe3cffe0c885ea46f1bc384
Valerii Voronkov <voronglass84@gmail.com>
2018-06-27 16:25:43 +0300

    Work on Industries table.

---
+++ admin/src/components/Table/RemoveAction.vue
@@ -0,0 +1,125 @@
+<template lang="pug">
+.errorsMessage(v-if="showRemoveWarningComp")
+    .message
+    span Do you want to delete data?
+    .buttonsBlock
+        button.confirm(@click="confirmRemoveComp(ind)") Confirm
+        button.cancel(@click="cancelRemoveComp") Cancel
+</template>
+
+<script>
+export default {
+  props: {
+    table: {
+      type: Object
+    },
+    indexToRemove: {
+      type: Number
+    },
+    showRemoveWarning: {
+      type: Boolean,
+      default: false
+    },
+    removeButtonDisable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      showRemoveWarningComp: this.showRemoveWarning,
+      indexToRemoveComp: this.indexToRemove,
+      removeButtonDisableComp: this.removeButtonDisable,
+      confRem: {
+        showRemoveWarningComp: false,
+        removeButtonDisableComp: false
+      }
+    };
+  },
+  methods: {
+    confirmRemoveComp(ind) {
+      this.showRemoveWarningComp = false;
+      let vari = this.indexToRemoveComp;
+      console.log(vari);
+      this.table.body.splice(vari, 1);
+      this.$emit("confirmFromRemove", this.confRem);
+    },
+    cancelRemoveComp() {
+      this.showRemoveWarningComp = false;
+      this.$emit("cancelFromRemove", this.confRem);
+    }
+  },
+  watch: {
+    indexToRemove() {
+      this.indexToRemoveComp = this.indexToRemove;
+    },
+    showRemoveWarning() {
+      this.showRemoveWarningComp = this.showRemoveWarning;
+    },
+    removeButtonDisable() {
+      this.removeButtonDisableComp = this.removeButtonDisable;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.errorsMessage {
+  width: 300px;
+  max-height: 160px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  background-color: white;
+  color: red;
+  z-index: 20;
+  border: 1px solid red;
+  box-shadow: 0 0 15px red;
+  text-align: center;
+  padding-bottom: 15px;
+  padding-top: 0;
+  font-size: 18px;
+  .message {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    span {
+      margin-top: 28px;
+      margin-bottom: 24px;
+    }
+    .buttonsBlock {
+      display: flex;
+      justify-content: space-around;
+      width: 100%;
+      .confirm,
+      .cancel {
+        border: 0;
+        width: 114px;
+        height: 40px;
+        border-radius: 12px;
+        background-color: #ff876c;
+        -webkit-box-shadow: 1px 1px 5px rgba(102, 86, 61, 0.6);
+        box-shadow: 1px 1px 5px rgba(102, 86, 61, 0.6);
+        font-size: 14px;
+        color: #fff;
+        outline: none;
+        cursor: pointer;
+      }
+    }
+  }
+}
+button {
+  width: 31px;
+  height: 34px;
+  background-color: #fff;
+  border: none;
+  background-repeat: no-repeat;
+  outline: none;
+  cursor: pointer;
+}
+</style>
