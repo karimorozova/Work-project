497ed0a81fa02ebb7bf28ea7e4ed3380f00f6682
dan <daniyal@wellyes.ru>
2018-09-14 11:05:52 +0300

    slots testing

--- vendor/components/freelanceApplication/Education.vue
+++ vendor/components/freelanceApplication/Education.vue
@@ -2,19 +2,32 @@
     .education
         .education__main-title EDUCATION
         span.education__comment If you have any queries regarding the completion of this form, please contact vendor@pangea.global.
-
+        EduTable(:heads="heads" :informations="informations")
+            thead(slot="header")
+                th(slot="headDeck" v-for="head in heads") {{ head }}
+            tbody(slot="tableBody")
+                tr(slot="row" v-for="info in informations")
+                    td(slot="rowDeck") {{ info.level }}
+                    td(slot="rowDeck") {{ info.field }}
+                    td(slot="rowDeck") {{ info.instruction }}
+                    td(slot="rowDeck") {{ info.grade }}
 </template>
 
 <script>
+import EduTable from "./education/EduTable";
 
 export default {
     data() {
         return {
-
+            heads: ["Study level", "Field", "Instruction", "Overall Grade"],
+            informations: [{level: "PHD", field: "Translation with Languages", instruction: "University of Harvard", grade: "98/100"}]
         }
     },
     methods: {
 
+    },
+    components: {
+        EduTable
     }
 }
 </script>
@@ -36,6 +49,25 @@ export default {
     }
     &__comment {
         font-size: 12px;
+    }
+}
+
+.education-table {
+    &__table {
+        td {
+            border: 1px solid #66563D;
+            padding: 5px;
+        }
+        th {
+            border-right: 1px solid #FFF;
+            background-color: #66563D;
+            color: #FFF;
+            padding: 5px;
+            font-weight: 400;
+            &:last-child {
+                border-right: 1px solid #66563D;
+            }
+        }
     }
 }
 
---
+++ vendor/components/freelanceApplication/education/EduTable.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+.education-table
+    table.education-table__table
+        slot(name="header")
+            slot(name="headDeck")
+        slot(name="tableBody")
+            slot(name="row")
+                slot(name="rowDeck")
+</template>
+
+<script>
+export default {
+    props: {
+        heads: {
+            type: Array
+        },
+        informations: {
+            type: Array
+        }
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.education-table__table {
+    border-collapse: collapse;
+    font-size: 14px;
+}
+
+</style>
