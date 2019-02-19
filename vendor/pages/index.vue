75ddfc004b56359e21735859a2f750ca74acacfe
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2019-02-19 09:28:54 +0300

    PAN-47 Vendor login page

--- vendor/pages/index.vue
+++ vendor/pages/index.vue
@@ -1,30 +1,69 @@
 <template lang="pug">
-    .main-page
-      span.main-page__title Coming soon...
+  .loginMain
+    .loginWrapper(v-if="forgotLink")
+      .imageWrapper
+        img(src="../assets/images/new-logo.png")
+      .loginForm
+        .labelWrapper
+          label.warningMessage(v-if="isLoginWarning") Check your email or password
+        .h2Wrapper
+          input.email(v-model='form.logemail' placeholder='Email' :class="{addShadow: form.logemail}")
+        .h2Wrapper
+          input.password(type="password" v-model='form.logpassword' placeholder='Password' :class="{addShadow: form.logpassword}")
+        .checkboxWrapper
+          input.checkboxWrapper__input(type="checkbox")
+          label.checkboxWrapper__label Remember me
+        .buttonWrapper
+          button(@click='sendForm' v-model='form.logemail, form.logpassword' :class="{changeButtonView: form.logemail && form.logpassword}") Sign In
+          h2(v-if='isLogin') You are logged in!
+        .formFooter
+          span.firstLabel(@click="forget") Forgot Your Password?
+    passwordrestore(v-else)
 </template>
-
 <script>
-export default {
-  data() {
-    return {
-    
+  import axios from "axios";
+  import PasswordRestore from '../components/PasswordRestore';
+  export default {
+    data() {
+      return {
+        form: {
+          logemail: "",
+          logpassword: ""
+        },
+        isLogin: false,
+        isLoginWarning: false,
+        forgotLink: true
+      };
+    },
+    methods: {
+      sendForm() {
+        this.$axios.post("/portal/auth", this.form).then(
+          response => {
+            console.log(response);
+            document.cookie = "ses=" + response.data.jsessionId + "; max-age=3600;" + "domain=pangea.global";
+            this.isLogin = true;
+            window.location.href = "/main";
+          },
+          err => {
+            alert("Bad credentials");
+            console.log("Errored : ");
+            console.log(err);
+          }
+        );
+      },
+      forget(){
+        this.forgotLink = !this.forgotLink;
+      }
+    },
+    computed: {},
+    mounted() {},
+    components: {
+      "passwordrestore": PasswordRestore
     }
-  },
-  methods: {
-    
-  },
-  mounted() {
-    
-  }
-};
+  };
 </script>
 
 <style lang="scss">
-
-.main-page {
-  &__title {
-    font-size: 24px;
-  }
-}
+  @import "../assets/styles/index.scss";
 
 </style>
