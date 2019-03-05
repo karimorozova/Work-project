b249866f64764203e690c5b517fc38527cf001ad
dan <daniyal@wellyes.ru>
2019-03-05 14:49:10 +0300

    - Added project-details page to closed jobs folder,  getting closed jobs

---
+++ vendor/pages/index/closed-jobs/project-details.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    .project-details    
+        ProjectDetails
+</template>
+
+<script>
+import ProjectDetails from "../../components/jobs/ProjectDetails";
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+    computed: {
+
+    },
+    components: {
+        ProjectDetails
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
