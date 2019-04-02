62597bc18cc2aa577c960166f4326d63947928a4
dan <daniyal@wellyes.ru>
2019-04-02 17:52:54 +0300

    - Added date range and averages in zoho reports

---
+++ admin/src/components/CheckBox.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+    .checkbox
+        img.checkbox__image(v-if="!isChecked" src="../assets/images/unselected-checkbox.png" @click="check")
+        img.checkbox__image(v-else src="../assets/images/selected-checkbox.png" @click="uncheck")   
+</template>
+
+<script>
+export default {
+    props: {
+        isChecked: {type: Boolean}
+    },
+    methods: {
+        check() {
+            this.$emit('check')
+        },
+        uncheck() {
+            this.$emit('uncheck')
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.checkbox {
+    &__image {
+        width: 18px;
+    }
+}
+
+</style>
