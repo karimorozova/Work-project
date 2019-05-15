f37a8668df741ac59f08b0171da86705071830bb
dan <daniyal@wellyes.ru>
2019-05-15 11:57:24 +0300

    - Fixed bug with getters in vendorrates

--- admin/src/components/vendors/VendorRates.vue
+++ admin/src/components/vendors/VendorRates.vue
@@ -78,7 +78,9 @@ export default {
         })
     },
     computed: {
-        vuexServices: "getVuexServices",
+        ...mapGetters({
+            vuexServices: "getVuexServices",
+        })
     },
     components: {
         DuoRates,
