ccbc11fd177c1c5d1c74681130a883c42c3ab23b
dan <daniyal@wellyes.ru>
2019-07-03 19:57:48 +0300

    - Testing projects 2

--- admin/src/components/pmArea/Allprojects.vue
+++ admin/src/components/pmArea/Allprojects.vue
@@ -140,7 +140,7 @@ export default {
                     }
                 })
             }
-            console.log("filtered ", filteredProjects);
+            console.log("filtered ", result);
             return result;
         }
     },
