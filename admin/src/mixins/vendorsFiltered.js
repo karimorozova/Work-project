2cc118403c149320ac1284f2345a9de4b3585f2b
dan <daniyal@wellyes.ru>
2019-10-07 12:31:58 +0300

    - Fixed Vendors table filtering

--- admin/src/mixins/vendorsFiltered.js
+++ admin/src/mixins/vendorsFiltered.js
@@ -2,34 +2,25 @@ export default {
     methods: {
         filterByStep(vendors) {
             return vendors.filter(vendor => {
-                const combs = vendor.languageCombinations;
+                const combs = [...vendor.wordsRates, ...vendor.hoursRates, ...vendor.monoRates];
                 return this.isActiveStep(combs);
             })
         },
         isActiveStep(combs) {
             for(let comb of combs) {
-                const { industries } = comb;
-                const activeStep = industries.find(item => {
-                    const { rates } = item;
-                    return this.checkServiceSteps(rates);
-                })
+                const { rates } = comb;
+                const activeStep = this.checkServiceSteps(rates);
                 if(activeStep) {
                     return true;
                 }
             }
         },
         checkServiceSteps(rates) {
-            const servicesWithStepFilter = this.services.filter(item => {
-                if(item.steps && item.steps.length) {
-                    return item.steps.find(stepItem => stepItem.step._id === this.stepFilter._id);
-                }
-            }).map(item => item._id);
             return Object.keys(rates).find(item => {
-                if(servicesWithStepFilter.indexOf(item) !== -1) {
+                if(item === this.stepFilter._id) {
                     return rates[item].active && +rates[item].value > 0;
                 }
-            })
-             
+            })             
         },
         async getAllServices() {
             try {
@@ -59,7 +50,7 @@ export default {
             }
             if(this.sourceFilter.length && this.sourceFilter[0] !== 'All') {
                 result = result.filter(item => {
-                    const combs = item.languageCombinations;
+                    const combs = [...item.wordsRates, ...item.hoursRates];
                     if(combs.length) {
                         const comb = combs.find(item => item.source && this.sourceFilter.indexOf(item.source.symbol) !== -1);
                         return comb;
@@ -68,7 +59,7 @@ export default {
             }
             if(this.targetFilter.length && this.targetFilter[0] !== 'All') {
                 result = result.filter(item => {
-                    const combs = item.languageCombinations;
+                    const combs = [...item.wordsRates, ...item.hoursRates, ...item.monoRates];;
                     if(combs.length) {
                         const comb = combs.find(item => this.targetFilter.indexOf(item.target.symbol) !== -1);
                         return comb;
