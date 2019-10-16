7dd1d952867c9853aff8351461dd0dca065a434b
dan <daniyal@wellyes.ru>
2019-10-16 16:31:16 +0300

    - Changed projects length condition for table body overflow

--- admin/src/components/pmArea/ProjectsTable.vue
+++ admin/src/components/pmArea/ProjectsTable.vue
@@ -3,8 +3,8 @@
     DataTable(
         :fields="fields"
         :tableData="allProjects"
-        :bodyClass="['all-projects', {'tbody_visible-overflow': allProjects.length < 12}]"
-        :tableheadRowClass="allProjects.length < 12 ? 'tbody_visible-overflow' : ''"
+        :bodyClass="['all-projects', {'tbody_visible-overflow': allProjects.length < 17}]"
+        :tableheadRowClass="allProjects.length < 17 ? 'tbody_visible-overflow' : ''"
         @onRowClicked="onRowClicked"
     )
         template(slot="headerProjectId" slot-scope="{ field }")
