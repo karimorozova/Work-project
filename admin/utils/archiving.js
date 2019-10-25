4d43c3da7e84f402cd6354eb46e79b697d900ea4
dan <daniyal@wellyes.ru>
2019-10-25 11:01:15 +0300

    - Used promise in archiver

--- admin/utils/archiving.js
+++ admin/utils/archiving.js
@@ -2,31 +2,30 @@ const fs = require("fs");
 const archiver = require("archiver");
 
 const archiveFile = ({outputPath, originFile}) => {
-    let output = fs.createWriteStream(outputPath);
-    let archive = archiver('zip', {
-        zlib: { level: 9 }
-    });
-    output.on('close', () => {
-        console.log(archive.pointer() + ' total bytes');
-        console.log('archiver has been finalized and the output file descriptor has closed.');
-    });
-    output.on('end', () => {
-        console.log('Data has been drained');
-    });
-    archive.on('warning', (err) => {
-        if (err.code === 'ENOENT') {
-            // log warning
-        } else {
-            throw err;
-        }
-    });
-    archive.on('error', (err) => {
-        throw err;
-    });
-    archive.pipe(output);
-    const file = originFile.path;
-    archive.append(fs.createReadStream(file), {name: originFile.filename});
-    archive.finalize();
+    return new Promise((resolve, reject) => {
+        let output = fs.createWriteStream(outputPath);
+        let archive = archiver('zip', {
+            zlib: { level: 9 }
+        });
+        output.on('close', () => {
+            console.log(archive.pointer() + ' total bytes');
+            console.log('archiver has been finalized and the output file descriptor has closed.');
+        });
+        output.on('end', () => {
+            console.log('Data has been drained');
+        });
+        archive.on('warning', (err) => {
+            reject(err);
+        });
+        archive.on('error', (err) => {
+            reject(err);
+        });
+        archive.pipe(output);
+        const file = originFile.path;
+        archive.append(fs.createReadStream(file), {name: originFile.filename});
+        archive.finalize();
+        resolve();
+    })
 }
 
 const archiveMultipleFiles = ({outputPath, files}) => {
