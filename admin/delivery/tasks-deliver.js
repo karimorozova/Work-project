c6d4930b9a827084b25e64159ee369b39a31c27d
dan <daniyal@wellyes.ru>
2019-12-12 14:42:23 +0300

    - Tasks deliver fixed(temporary decision)

--- admin/delivery/tasks-deliver.js
+++ admin/delivery/tasks-deliver.js
@@ -5,7 +5,12 @@ async function getAfterTasksDelivery(tasks) {
     const taskIds = tasks.map(item => item.taskId)
     try {
         const project = await getProject({"tasks.taskId": taskIds[0]})
-        return await getProjectAfterApprove({taskIds, project, isDeliver: true});
+        const contacts = project.customer.contacts.filter(item => item.leadContact).map(item=> item.email);
+        let updatedProject = null;
+        for(let taskId of taskIds) {
+            updatedProject = await getProjectAfterApprove({taskId, project, isDeliver: true, contacts});
+        }
+        return updatedProject;
     } catch(err) {
         console.log(err);
         console.log("Error in getAfterTasksDelivery");
