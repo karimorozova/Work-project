662ebde82d02b51b7e9ce80e94a82cab86cb7761
dan <daniyal@wellyes.ru>
2020-01-15 19:34:53 +0300

    - Wrapped emitter body with try/catch

--- admin/events/index.js
+++ admin/events/index.js
@@ -5,16 +5,24 @@ const { sendEmail, notifyManagerProjectStarts } = require('../utils');
 
 emitter.on('testEvent', () => {
     setTimeout(async () => {
-        const vendors = await Vendors.find();
-        const message = 'Some message here';
-        await sendEmail({to: vendors[0].email, subject: 'Just a test email'}, message);
-        console.log('emitted');
+        try {
+            const vendors = await Vendors.find();
+            const message = 'Some message here';
+            await sendEmail({to: vendors[0].email, subject: 'Just a test email'}, message);
+            console.log('emitted');
+        } catch(err) {
+            console.log("Error from emitter testEvent");
+            console.log(err);
+        }
     }, 60000) 
 });
 
 emitter.on('managersNotificationEmail', async (project) => {
-    if(project.isStartAccepted) {
+    try {
         await notifyManagerProjectStarts(project);
+    } catch(err) {
+        console.log("Error from emitter managersNotificationEmail");
+        console.log(err);
     }
 })
 
