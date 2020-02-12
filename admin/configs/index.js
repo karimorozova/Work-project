0e320c59f96461c5842dccbcdecdd41ccbf698a5
dan <daniyal@wellyes.ru>
2020-02-12 10:08:17 +0300

    - Added memoq configs

--- admin/configs/index.js
+++ admin/configs/index.js
@@ -1,10 +1,14 @@
 const { xtmToken, xtmBaseUrl } = require('./xtm');
 const secretKey = require('./jwtkey');
 const zohoCreds = require('./zoho');
+const { xmlHeader, getHeaders, createCORSRequest } = require('./memoq');
 
 module.exports = {
     xtmToken,
     xtmBaseUrl,
     secretKey,
-    zohoCreds
+    zohoCreds,
+    xmlHeader, 
+    getHeaders,
+    createCORSRequest
 }
