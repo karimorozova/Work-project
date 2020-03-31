e0277253c51f858dd3f679ebdd8a834908a8a0d3
PLMR <maxyplmr@gmail.com>
2020-03-31 09:41:51 +0300

    - Update ClientCommunication

--- admin/emailMessages/clientCommunication.js
+++ admin/emailMessages/clientCommunication.js
@@ -5,9 +5,15 @@ const { secretKey } = require('../configs');
 function messageForClient(obj) {
     const date = Date.now();
     const name = `${obj.contact.firstName} ${obj.contact.surname}`;
-    const tasksInfo = getTasksInfo({ tasks: obj.tasks, industry: obj.industry });
-    const token = jwt.sign({ id: obj.id }, secretKey, { expiresIn: '2h' });
-    const detailHeader = obj.status === 'Quote sent' ? "Your quote has been updated - please see below the quote details:" : "Please see below the quote details:";
+    const tasksInfo = getTasksInfo({tasks: obj.tasks, industry: obj.industry});
+    const token = jwt.sign({id: obj.id}, secretKey, { expiresIn: '2h'});
+    let detailHeader = "Please see below the quote details:";
+    if(obj.isPriceUpdated) {
+        detailHeader = obj.status === "Quote sent" ? 
+                "Your quote has been updated - please see below the quote details:" 
+                : `Your project ${obj.projectId} - ${obj.projectName} had a change in it's price.`;
+    }
+    const reason = obj.reason ? `<p>Reason ${obj.reason}</p><p>Please see below the updated quote details</p>` : ""; 
     const acceptQuote = '<a href=' + `${apiUrl}/projectsapi/acceptquote?projectId=${obj.id}&to=${date}&t=${token}` + ` target="_blank" style="color: orange;">I accept - ${obj.projectId}, ${obj.finance.Price.receivables} &euro;</a>`
     const declineQuote = '<a href=' + `${apiUrl}/projectsapi/declinequote?projectId=${obj.id}&to=${date}t=${token}` + ` target="_blank" style="color: orange;">I reject - ${obj.projectId}, ${obj.finance.Price.receivables} &euro;</a>`
     return `<div class="wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
@@ -22,6 +28,7 @@ function messageForClient(obj) {
             <p class="description" style="font-size: 18px;">
                 ${detailHeader}
             </p>
+            ${reason}
             <h3 class="detailsTitle">Quote Details</h3>
             <table class="details" style="width: 600px;box-sizing: border-box;border: 1px solid rgb(129, 129, 129);border-collapse: collapse;">
                 <tr>
@@ -70,7 +77,7 @@ function getTasksInfo(info) {
         return acc;
     }, {})
     let result = "";
-    for (let key in services) {
+    for(let key in services) {
         const tasksInfo = services[key].reduce((acc, cur) => {
             const deadline = acc['deadline'] || cur.deadline;
             const langPair = cur.sourceLanguage ? `${cur.sourceLanguage} >> ${cur.targetLanguage}` : `${cur.targetLanguage} / ${cur.packageSize}`;
@@ -79,7 +86,7 @@ function getTasksInfo(info) {
             acc['cost'] = acc['cost'] ? acc['cost'] + cur.finance.Price.receivables : cur.finance.Price.receivables;
             return acc;
         }, {})
-        result += getTaskCode({ ...tasksInfo, service: key, industry });
+        result+= getTaskCode({...tasksInfo, service: key, industry});
     }
     return result;
 }
@@ -195,7 +202,7 @@ function taskDeliveryMessage(obj) {
                         I'm pleased to inform you that the task ${obj.task.taskId} (${obj.task.service.title}) from your project ${obj.projectId} - ${obj.projectName} has been completed and is ready for review.
                     </p>
                     <p class="description" style="font-size: 18px;">
-                        The files are available for you in our <a href="https://portal.pangea.global">Protal</a> and attached to this email in a zip format.
+                        The files are available for you in our <a href="https://portal.pangea.global">Portal</a> and attached to this email in a zip format.
                     </p>
                     <p>
                         In case of any questions, please do not hesitate to contact us :-)
@@ -205,46 +212,18 @@ function taskDeliveryMessage(obj) {
 }
 
 function tasksQuoteMessage(obj) {
-    const langPair = obj.task.sourceLanguage ? `${obj.task.sourceLanguage} >> ${obj.task.targetLanguage}` : `${obj.task.targetLanguage} / ${obj.task.packageSize}`;
-    const receivables = obj.task.finance.Price.receivables;
     return `<div contenteditable="true" class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);overflow-y: auto">
             <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.contact.firstName} ${obj.contact.surname},</h3>
             <div class="all-info" style="padding: 0 15px 0 30px;">
                 <p class="description" style="font-size: 18px;">
-                    Please find attached Quote(s) for existing project.
+                    The new task added to your project ${obj.projectId} - ${obj.projectName}.
                 </p>
-                <h3 class="detailsTitle">Quote(s) Details</h3>
-                <table class="details">
-                    <tr>
-                        <td>Project number:</td>
-                        <td>${obj.projectId}</td>
-                    </tr>
-                    <tr>
-                        <td>Project name:</td>
-                        <td>${obj.projectName}</td>
-                    </tr>
-                    <tr>
-                        <td>Service:</td>
-                        <td>${obj.service}</td>
-                    </tr>
-                    <tr>
-                        <td>Language pair:</td>
-                        <td>${langPair}</td>
-                    </tr>
-                    <tr>
-                        <td>Specialization:</td>
-                        <td>${obj.industry.name}</td>
-                    </tr>
-                    <tr>
-                        <td>Amount:</td>
-                        <td>${receivables} &euro;</td>
-                    </tr>
-                </table>
+                <p class="description" style="font-size: 18px;">
+                    Task ${obj.task.taskId} - ${obj.task.service.title}.
+                </p>   
                 <p>
-                    In case of any questions, please do not hesitate to contact us.
+                    You can track the progress on the <a href="https://portal.pangea.global">Portal</a>.
                 </p>
-                <h2 class="contact" style="border-bottom: 1px solid rgb(29, 29, 29);">Contact Pangea Translation Services (Cyprus) LTD</h2>
-                <a href="http://pangea.global" target="_blank"><img src="cid:logo@pan" style="width: 50%; margin-left: 145px;"></a>
             </div>
         </div>`;
 }
@@ -295,69 +274,27 @@ function tasksCancelledMessage(obj) {
         </div>`;
 }
 
-function decideQuote(obj) {
-    return `<div class="wrapper" style="width:800px;border-width:1px;border-style:solid;border-color:rgb(129, 129, 129);font-family:'Roboto', sans-serif;color:#66563E;box-sizing:border-box;" >
-    <header style="background-color:#66563E;text-align:center;" >
-        <img class="logo" src="../static/email-logo.png" alt="pangea" style="margin-top:20px;margin-bottom:20px;margin-right:0;margin-left:0;" >
-    </header>
-    <div class="main" style="padding-top:40px;padding-bottom:40px;padding-right:40px;padding-left:40px;" >
-        <h4 class="contact-name">Dear ${obj.contact.firstName} ${obj.contact.surname}</h4>
-        <p class="main_italic main_line15 main_weight600" style="font-weight:600;font-style:italic;margin-top:10px;margin-bottom:40px;margin-right:0;margin-left:0;line-height:1.5;" >***This is an automated message***<br>
-            This message is sent to you on behalf of ${obj.accManager.firstName} ${obj.accManager.lastName}</p>
-        <p>Please see below the quote details:</p>
-        <div class="details" style="width:90%;margin-top:0;margin-bottom:0;margin-right:auto;margin-left:auto;" >
-            <h4 class="details__title">Quote Details</h4>
-            <table class="details__table" style="color:#66563E;border-width:1px;border-style:solid;border-color:#66563E;border-collapse:collapse;" >
-                <tr>
-                    <td class="main_weight600" style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;font-weight:600;" >Name:</td>
-                    <td style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;" >${obj}</td>
-                </tr>
-                <tr>
-                    <td class="main_weight600" style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;font-weight:600;" >ID:</td>
-                    <td style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;" >${obj}</td>
-                </tr>
-                <tr>
-                    <td class="main_weight600" style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;font-weight:600;" >Service:</td>
-                    <td style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;" >${obj}</td>
-                </tr>
-                <tr>
-                    <td class="main_weight600" style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;font-weight:600;" >Language(s):</td>
-                    <td style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;" >${obj}</td>
-                </tr>
-                <tr>
-                    <td class="main_weight600" style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;font-weight:600;" >Industry:</td>
-                    <td style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;" >${obj}</td>
-                </tr>
-                <tr>
-                    <td class="main_weight600" style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;font-weight:600;" >Estimated delivery date:</td>
-                    <td style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;" >${obj}</td>
-                </tr>
-                <tr>
-                    <td class="main_weight600" style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;font-weight:600;" >Cost:</td>
-                    <td style="border-width:1px;border-style:solid;border-color:#66563E;padding-top:5px;padding-bottom:5px;padding-right:5px;padding-left:5px;min-width:200px;" >${obj}</td>
-                </tr>
-            </table>
-        </div>
-        <p class="main_weight600 main_line15" style="font-weight:600;line-height:1.5;" >
-            By clicking on one of the link below, you can accept or reject our offer.<br>
-            <span class="main_line15-red" style="background-color:#FBF3DB;padding-top:2px;padding-bottom:2px;padding-right:0;padding-left:0;" >
-                Clicking "I accept" will also approve and accept our <a href="https://www.pangea.global/wp-content/uploads/2019/11/Pangea-Terms-Conditions.pdf" class="link" style="color:#D15F46;" >terms and conditions</a>
-            </span>
-        </p>
-        <p>You can accept the quote by clicking the link below:</p>
-        <a class="link" href="${acceptQuote}" style="color:#D15F46;" >I accept - ${obj.project.projectId} - ${obj.project.projectName}</a>
-        <p>or</p>
-        <a class="link" href="${declineQuote}" style="color:#D15F46;" >I reject - ${obj.project.projectId} - ${obj.project.projectName}</a>
-        <p><span class="main_weight600 main_line15" style="font-weight:600;line-height:1.5;" >Please note:</span>once accepting the quote, the project will start
-            automatically.<br>
-            In case of any questions, please do not hesitate to contact us :-)</p>
-    </div>
-    <footer>
-        <hr size="15" color="#66563E">
-        <a class="footer__link" href="https://www.pangea.global" style="display:block;width:100%;text-align:center;padding-top:10px;padding-bottom:15px;padding-right:0;padding-left:0;text-decoration:none;color:#66563E;" >www.pangea.global</a>
-    </footer>
-</div>`;
-
+function projectDeliveryMessage(obj) {
+    return `<div class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
+                <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.contact.firstName} ${obj.contact.surname},</h3>
+                <div class="all-info" style="padding: 0 15px 0 30px;">
+                    <p class="comment" style="font-style: italic;font-weight: bold;">
+                        ***This is an automated message*** 
+                    </p>
+                    <p class="comment" style="font-style: italic;font-weight: bold;">
+                        This message is sent to you on behalf of ${obj.accManager.firstName} ${obj.accManager.lastName}.
+                    </p>
+                    <p class="description" style="font-size: 18px;">
+                        I'm pleased to inform you that project ${obj.projectId} - ${obj.projectName} has been completed and is ready for review.
+                    </p>
+                    <p class="description" style="font-size: 18px;">
+                        The files are available for you in our <a href="https://portal.pangea.global">Portal</a> and attached to this email in a zip format.
+                    </p>
+                    <p>
+                        In case of any questions, please do not hesitate to contact us :-)
+                    </p>
+                </div>
+            </div>`;
 }
 
 module.exports = {
@@ -368,5 +305,5 @@ module.exports = {
     tasksQuoteMessage,
     projectCancelledMessage,
     tasksCancelledMessage,
-    decideQuote
+    projectDeliveryMessage
 }
