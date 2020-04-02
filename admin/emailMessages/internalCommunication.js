2d4813da5aaa957e48a6abde728019b28b5236e4
PLMR <maxyplmr@gmail.com>
2020-04-02 19:03:00 +0300

    - Internal Communication

--- admin/emailMessages/internalCommunication.js
+++ admin/emailMessages/internalCommunication.js
@@ -49,39 +49,14 @@ function managerTaskCompleteNotificationMessage(obj) {
             <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.projectManager.firstName} ${lastName},</h3>
             <div class="all-info" style="padding: 0 15px 0 30px;">
                 <p class="description" style="font-size: 18px;">
-                    Please, pay attention that one of the tasks has been completed.
+                    Task ${obj.task.taskId} from project ${obj.projectId} - ${obj.projectName} has been completed and ready for DR1.
                 </p>
-                <h3 class="detailsTitle">See details below:</h3>
-                <table class="details">
-                    <tr>
-                        <td>Project number:</td>
-                        <td>${obj.projectId}</td>
-                    </tr>
-                    <tr>
-                        <td>Project name:</td>
-                        <td>${obj.projectName}</td>
-                    </tr>
-                    <tr>
-                        <td>Task ID:</td>
-                        <td>${obj.task.taskId}</td>
-                    </tr>
-                    <tr>
-                        <td>Service:</td>
-                        <td>${obj.service}</td>
-                    </tr>
-                    <tr>
-                        <td>Language pair:</td>
-                        <td>${pair}</td>
-                    </tr>
-                    <tr>
-                        <td>Start date: </td>
-                        <td>${moment(obj.task.start).format('DD-MM-YYYY')}</td>
-                    </tr>
-                    <tr>
-                        <td>Deadline: </td>
-                        <td>${moment(obj.task.deadline).format('DD-MM-YYYY')}</td>
-                    </tr>
-                </table>
+                <p class="description" style="font-size: 18px;">
+                    Project deadline is: ${obj.deadline}
+                </p>
+                <p class="description" style="font-size: 18px;">
+                    Please, make the review or assign another PM to do it.
+                </p>
             </div>
         </div>`;
 }
@@ -110,6 +85,50 @@ function deliverablesDownloadedMessage(obj) {
 }
 
 function managerRequestNotifyingMessage(obj) {
+    const industry = obj.industry ? obj.industry.name : "";
+    const { sourceLanguage, packageSize } = obj;
+    const languages = obj.targetLanguages.reduce((acc, cur) => {
+        acc+= sourceLanguage ? `${sourceLanguage.lang} >> ${cur.lang}; ` : `${cur.lang}/${packageSize.size}; `
+        return acc; 
+    }, "")
+    return `<div class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
+            <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.user.firstName},</h3>
+            <div class="all-info" style="padding: 0 15px 0 30px;">
+                <p class="description" style="font-size: 18px;">
+                    Client ${obj.customer.name} has sent a request. 
+                </p>
+                <h3 class="detailsTitle">Please, find the details below:</h3>
+                <table class="details">
+                    <tr>
+                        <td>Request name:</td>
+                        <td>${obj.projectName}</td>
+                    </tr>
+                    <tr>
+                        <td>Request ID:</td>
+                        <td>${obj.requestId}</td>
+                    </tr>
+                    <tr>
+                        <td>Service:</td>
+                        <td>${obj.service.title}</td>
+                    </tr>
+                    <tr>
+                        <td>Languages:</td>
+                        <td>${languages}</td>
+                    </tr>
+                    <tr>
+                        <td>Industry:</td>
+                        <td>${industry}</td>
+                    </tr>
+                    <tr>
+                        <td>Estimated delivery date: </td>
+                        <td>${obj.deadline}</td>
+                    </tr>
+                </table>
+            </div>
+            </div>`;
+}
+
+function managerRequestAssignedMessage(obj) {
     return `<div class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
             <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.user.firstName},</h3>
             <div class="all-info" style="padding: 0 15px 0 30px;">
@@ -125,10 +144,6 @@ function managerRequestNotifyingMessage(obj) {
                     <tr>
                         <td>Request name:</td>
                         <td>${obj.projectName}</td>
-                    </tr>
-                    <tr>
-                        <td>Start date: </td>
-                        <td>${obj.startDate}</td>
                     </tr>
                     <tr>
                         <td>Suggested Deadline: </td>
@@ -181,12 +196,90 @@ function managerProjectRejectedMessage(obj) {
         </div>`;
 }
 
+<<<<<<< HEAD
 module.exports = {
+=======
+function stepStartedMessage(obj) {
+    const lastName = obj.projectManager.lastName || "";
+    const vendorSurname = obj.step.vendor.surname || "";
+    return `<div class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
+            <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.projectManager.firstName} ${lastName},</h3>
+            <div class="all-info" style="padding: 0 15px 0 30px;">
+                <p class="description" style="font-size: 18px;">
+                    Vendor ${obj.step.vendor} ${vendorSurname} has just started the step: ${obj.step.stepId} from ${obj.projectId} - ${obj.projectName}. You can track progress on Project page.
+                </p>
+            </div>
+            </div>`
+}
+
+function stepCompletedMessage(obj) {
+    const lastName = obj.accountManager.lastName || "";
+    return `<div class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
+            <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.accountManager.firstName} ${lastName},</h3>
+            <div class="all-info" style="padding: 0 15px 0 30px;">
+                <p class="description" style="font-size: 18px;">
+                    Vendor ${obj.step.vendor} ${vendorSurname} has just completed the step: ${obj.step.stepId} from ${obj.projectId} - ${obj.projectName}.
+                </p>
+            </div>
+            </div>`
+}
+
+function requestCancelledMessage(obj) {
+    const lastName = obj.accountManager.lastName || "";
+    return `<div class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
+            <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.accountManager.firstName} ${lastName},</h3>
+            <div class="all-info" style="padding: 0 15px 0 30px;">
+                <p class="description" style="font-size: 18px;">
+                    Client ${obj.customer.name} has cancelled the request: ${obj.requestId} - ${obj.projectName}.
+                </p>
+            </div>
+            </div>`
+}
+
+function  stepDecisionMessage(obj) {
+    const lastName = obj.accountManager.lastName || "";
+    const decision = obj.decision === "accept" ? "approved" : "rejected";
+    return `<div class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
+            <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.accountManager.firstName} ${lastName},</h3>
+            <div class="all-info" style="padding: 0 15px 0 30px;">
+                <p class="description" style="font-size: 18px;">
+                    Vendor ${obj.step.vendor} ${vendorSurname} has ${decision} the assigned step: ${obj.step.stepId} from ${obj.projectId} - ${obj.projectName} Project.
+                </p>
+            </div>
+            </div>`
+}
+
+function readyForDr2Message(obj) {
+    const lastName = obj.dr2Manager.lastName || "";
+    return `<div class="message-wrapper" style="width: 960px;border: 1px solid rgb(129, 129, 129);">
+            <h3 class="clientName" style="margin-top: 0;padding: 30px;background-color: rgb(250, 250, 250);">Dear ${obj.dr2Manager.firstName} ${lastName},</h3>
+            <div class="all-info" style="padding: 0 15px 0 30px;">
+                <p class="description" style="font-size: 18px;">
+                    The Delivery Review for ${taskId} from project${obj.projectId} - ${obj.projectName} has been finished.
+                </p>
+                <p class="description" style="font-size: 18px;">
+                    Please, do the Delivery Review 2.
+                </p>
+                <p class="description" style="font-size: 18px;">
+                    Project deadline is: ${obj.deadline}
+                </p>
+            </div>
+            </div>`;
+}
+
+module.exports = { 
+>>>>>>> master
     getMessageWithRandomPassword,
     managerAssignmentNotifyingMessage,
     managerTaskCompleteNotificationMessage,
     deliverablesDownloadedMessage,
     managerRequestNotifyingMessage,
+    managerRequestAssignedMessage,
     managerProjectAcceptedMessage,
-    managerProjectRejectedMessage
+    managerProjectRejectedMessage,
+    stepStartedMessage,
+    stepCompletedMessage,
+    requestCancelledMessage,
+    stepDecisionMessage,
+    readyForDr2Message
 };
