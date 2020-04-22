cea69b1e39852d0bfca68486b10e6c00b25735ab
PLMR <maxyplmr@gmail.com>
2020-04-22 16:07:21 +0300

    - Vendor Preview in client-communication

--- admin/src/components/vendors/VendorPreview.vue
+++ admin/src/components/vendors/VendorPreview.vue
@@ -1,20 +1,22 @@
 <template lang="pug">
-.preview
-    span.preview__close(@click="closePreview") +
-    .preview__header
-        .preview__drop-menu
-            SelectSingle(
-                placeholder="Choose Email Template"
-                :selectedOption="currentTemplate"
-                :options="templates"
-                @chooseOption="setTemplate"
-                @scrollDrop="scrollDrop"
-            )
-        .preview__title Quote Preview
-    .preview__details
-        ckeditor(v-model="editorData" :config="editorConfig")
-    .preview__button
-        Button(value="Send" @clicked="send")
+div
+  .preview
+      span.preview__close(@click="closePreview") +
+      .preview__header
+          .preview__drop-menu
+              SelectSingle(
+                  placeholder="Choose Email Template"
+                  :selectedOption="currentTemplate"
+                  :options="templates"
+                  @chooseOption="setTemplate"
+                  @scrollDrop="scrollDrop"
+              )
+          .preview__title Preview
+      .preview__details
+          ckeditor(v-model="editorData" :config="editorConfig")
+      .preview__button
+          Button(value="Send" @clicked="send")
+  .background
 </template>
 
 <script>
@@ -33,9 +35,12 @@ export default {
   data() {
     return {
       isTableDropMenu: true,
-      templates: ["None","Test template1", "Test template2", "Test template3"],
+      templates: ["None", "Test template1", "Test template2", "Test template3"],
       currentTemplate: "",
       editorData: this.message,
+      editorConfig: {
+        allowedContent: true
+      }
     };
   },
   methods: {
@@ -49,23 +54,43 @@ export default {
       this.currentTemplate = this.templates.find(item => item === option);
     }
   },
+  mounted() {
+    document.body.style.overflow = "hidden"
+  },
+  destroyed() {
+    document.body.style.overflow = "auto"
+  },
   components: {
     Button,
     SelectSingle,
     ckeditor: CKEditor.component
-  },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../../assets/scss/colors.scss";
-
+.background{
+  position: fixed;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    opacity: 0.5;
+    overflow: hidden;
+}
 .preview {
-  position: sticky;
+  position: fixed;
+  z-index: 200;
   background-color: $white;
-  min-width: 80%;
-  top: 10%;
-  margin-left: 10%;
+  min-width: 1000px;
+  max-width: 100%;
+  top: 20%;
+  left: 20%;
+  right: 20%;
+  margin: 0 auto;
   padding: 20px;
   box-shadow: 0 0 10px $brown-shadow;
   display: flex;
