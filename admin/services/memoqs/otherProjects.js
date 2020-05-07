a8118582c770858353fd94beb8ca18e7ae899da6
Inheritor <peysho@Mail.ru>
2020-05-07 18:44:34 +0300

    Add lazy-load functionality on backend

--- admin/services/memoqs/otherProjects.js
+++ admin/services/memoqs/otherProjects.js
@@ -2,14 +2,31 @@ const { MemoqProject } = require('../../models');
 
 function getFilteredProjectQuery(filters) {
   let query = {};
+  if(filters.lastDate) {
+    query.creationTime = {$lt: new Date(filters.lastDate)};
+  }
   if (filters.clientFilter) {
     query.client = { '$regex': new RegExp(`${filters.clientFilter}`, 'i') };
   }
   if (filters.startFilter) {
-    query.creationTime = { $gte: new Date(filters.startFilter) };
+    query.creationTime = filters.lastDate ? {$lt: new Date(filters.lastDate), $gte: new Date(filters.startFilter)} : {$gte: new Date(filters.startFilter)};
   }
   if (filters.deadlineFilter) {
     query.deadline = { $lte: new Date(filters.deadlineFilter) };
+  }
+  if (filters.pmFilter) {
+    query['$and'] =
+      [
+        {
+          'users.User.FullName':
+            {
+              '$regex': new RegExp(`${filters.pmFilter}`, 'i')
+            }
+        },
+        {
+          'users.ProjectRoles.isPm': true,
+        }
+      ]
   }
   if (filters.sourceFilter && filters.sourceFilter.length) {
     query['sourceLanguage.symbol'] = { '$in': filters.sourceFilter }
@@ -22,8 +39,9 @@ function getFilteredProjectQuery(filters) {
 
 async function getFilteredOtherProjects(filters) {
   try {
-    const filteredProjects = await getFilteredProjectQuery(filters);
-    return MemoqProject.find(filteredProjects);
+    const filteredProjects = getFilteredProjectQuery(filters);
+    result = await MemoqProject.find(filteredProjects).sort({ creationTime: -1 }).limit(25)
+    return result;
   } catch (err) {
     console.log(err.message);
     console.log('Error in getFilteredOtherProjects');
