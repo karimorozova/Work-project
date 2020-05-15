c4e794b5265287c70df8759a710df66f0412355f
Miras <peysho@mail.ru>
2020-05-15 15:16:15 +0300

    Add new languages, add vendors update functionality

--- admin/app.js
+++ admin/app.js
@@ -13,6 +13,7 @@ const checkCollections = require("./helpers/dbSetDefault");
 const { checkRoutes } = require("./middleware/index");
 const history = require('connect-history-api-fallback');
 let logger = require('morgan');
+const updateVendors = require('./helpers/defaults/vendors.js');
 
 const { updateMemoqProjectsData } = require('./services/memoqs/projects');
 const { getLangReports } = require('./reports/langReport');
@@ -37,7 +38,6 @@ schedule.scheduleJob('30 23 * * *', async function() {
         console.log(err.message);
     }
 })
-
 
 const allowedOrigins = [
   "https://admin.pangea.global",
