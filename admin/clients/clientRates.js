210179f1d1cd9839c77673ef03c5cdd9b9a168a1
Miras <peysho@mail.ru>
2020-07-07 16:12:03 +0300

    Add client's service uniqueness check, add functionality for creating new combinations with new services

--- admin/clients/clientRates.js
+++ admin/clients/clientRates.js
@@ -1,4 +1,5 @@
 const { Clients } = require('../models');
+const _ = require('lodash');
 
 const updateClientRates = async (clientId, itemIdentifier, updatedItem) => {
   const client = await Clients.findOne({ _id: clientId });
@@ -7,15 +8,18 @@ const updateClientRates = async (clientId, itemIdentifier, updatedItem) => {
     default:
     case 'Basic Price Table':
       const updatedBasicPriceTable = replaceOldItem(basicPricesTable, updatedItem);
-      await Clients.updateOne({ _id: clientId }, { basicPricesTable: updatedBasicPriceTable });
+      client.rates.basicPricesTable = updatedBasicPriceTable;
+      await Clients.updateOne({ _id: clientId }, { rates: client.rates });
       break;
     case 'Step Multipliers Table':
       const updatedStepMultipliersTable = replaceOldItem(stepMultipliersTable, updatedItem);
-      await Clients.updateOne({ _id: clientId }, { stepMultipliersTable: updatedStepMultipliersTable });
+      client.rates.stepMultipliersTable = updatedStepMultipliersTable;
+      await Clients.updateOne({ _id: clientId }, { rates: client.rates });
       break;
     case 'Industry Multipliers Table':
       const updatedIndustryMultipliersTable = replaceOldItem(industryMultipliersTable, updatedItem);
-      await Clients.updateOne({ _id: clientId }, { industryMultipliersTable: updatedIndustryMultipliersTable });
+      client.rates.industryMultipliersTable = updatedIndustryMultipliersTable;
+      await Clients.updateOne({ _id: clientId }, { rates: client.rates });
       break;
   }
 };
@@ -23,9 +27,54 @@ const updateClientRates = async (clientId, itemIdentifier, updatedItem) => {
 const replaceOldItem = (arr, replacementItem) => {
   const { _id } = replacementItem;
   const itemToUpdateIndex = findIndexToReplace(arr, _id);
-  return arr.splice(itemToUpdateIndex, 1, replacementItem);
+  arr.splice(itemToUpdateIndex, 1, replacementItem);
+  return arr;
 };
 
-const findIndexToReplace = (arr, searchItemId) => arr.findIndex(item => item._id === searchItemId);
+const findIndexToReplace = (arr, searchItemId) => arr.findIndex(item => item._id.toString() === searchItemId);
 
-module.exports = { updateClientRates };
+//TODO: Add source-language existence check
+const addNewRateComponents = async (clientId, newObj) => {
+  const { sourceLanguage, targetLanguage, service, industry } = newObj;
+  const client = await Clients.findOne({ _id: clientId });
+  const { basicPricesTable, stepMultipliersTable, industryMultipliersTable } = client.rates;
+  basicPricesTable.push({
+    type: 'Duo',
+    sourceLanguage: sourceLanguage._id,
+    targetLanguage: targetLanguage._id
+  });
+  stepMultipliersTable.push(...getStepMultipliersCombinations(service));
+  industryMultipliersTable.push({
+    industry: industry._id
+  });
+};
+
+//TODO: Add clients currencies for combinations
+const getStepMultipliersCombinations = (step) => {
+  const { calculationUnit } = step;
+  if (!calculationUnit.length) {
+    return [];
+  } else {
+    const stepUnitSizeCombinations = [];
+    for (let { _id, sizes } of calculationUnit) {
+      if (sizes.length) {
+        sizes.forEach(size => {
+          stepUnitSizeCombinations.push({
+            step: step._id,
+            unit: _id,
+            size
+          });
+        });
+      } else {
+        stepUnitSizeCombinations.push({
+          step: step._id,
+          unit: _id,
+          size: 1
+        });
+      }
+    }
+    return stepUnitSizeCombinations;
+  }
+};
+
+module.exports = { updateClientRates, addNewRateComponents };
--- admin/clients/clientService.js
+++ admin/clients/clientService.js
@@ -1,26 +1,27 @@
 const { Clients } = require('../models');
+const { addNewRateComponents } = require('./clientRates');
 
 const updateClientService = async (clientId, dataToUpdate) => {
   try {
     let { services } = await Clients.findOne({ _id: clientId });
-
-    if(dataToUpdate._id){
+    if (dataToUpdate._id) {
       const neededServiceIndex = services.findIndex(service => service._id === dataToUpdate._id);
       services.splice(neededServiceIndex, 1, dataToUpdate);
-    }else{
-      services.push(dataToUpdate)
+    } else {
+      const sameObject = findSameObject(dataToUpdate, services);
+      if (!sameObject) {
+        await addNewRateComponents(clientId, dataToUpdate);
+      }
+      services.push(dataToUpdate);
     }
-
     await Clients.updateOne({ _id: clientId }, { services });
   } catch (err) {
     console.log(err);
     console.log('Error in updateClientService');
   }
-}
+};
 
 const deleteClientService = async (clientId, serviceId) => {
-  console.log(serviceId);
-  
   try {
     const { services } = await Clients.findOne({ _id: clientId });
     const neededServiceIndex = services.findIndex(service => service._id === serviceId);
@@ -30,6 +31,16 @@ const deleteClientService = async (clientId, serviceId) => {
     console.log(err);
     console.log('Error in deleteClientService');
   }
-}
+};
+
+// TODO: Add alternative search without source language
+const findSameObject = (newObject, arrOfOldObjects) => {
+  return arrOfOldObjects.find(item => (
+    item.sourceLanguage.toString() === newObject.sourceLanguage._id &&
+    item.targetLanguage.toString() === newObject.targetLanguage._id &&
+    item.service.toString() === newObject.service._id &&
+    item.industry.toString() === newObject.industry._id
+  ));
+};
 
 module.exports = { updateClientService, deleteClientService };
