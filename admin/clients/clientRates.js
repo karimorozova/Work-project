63a0a743749dc49591a32e3001457f0d4e934e6f
PLMR <maxyplmr@gmail.com>
2020-07-07 15:54:01 +0300

    Update clientRates.js

--- admin/clients/clientRates.js
+++ admin/clients/clientRates.js
@@ -7,25 +7,30 @@ const updateClientRates = async (clientId, itemIdentifier, updatedItem) => {
     default:
     case 'Basic Price Table':
       const updatedBasicPriceTable = replaceOldItem(basicPricesTable, updatedItem);
-      await Clients.updateOne({ _id: clientId }, { basicPricesTable: updatedBasicPriceTable });
+      client.rates.basicPricesTable = updatedBasicPriceTable
+      await Clients.updateOne({ _id: clientId }, { rates: client.rates });
       break;
     case 'Step Multipliers Table':
       const updatedStepMultipliersTable = replaceOldItem(stepMultipliersTable, updatedItem);
-      await Clients.updateOne({ _id: clientId }, { stepMultipliersTable: updatedStepMultipliersTable });
+      client.rates.stepMultipliersTable = updatedStepMultipliersTable
+      await Clients.updateOne({ _id: clientId }, { rates: client.rates });
       break;
     case 'Industry Multipliers Table':
       const updatedIndustryMultipliersTable = replaceOldItem(industryMultipliersTable, updatedItem);
-      await Clients.updateOne({ _id: clientId }, { industryMultipliersTable: updatedIndustryMultipliersTable });
+      client.rates.industryMultipliersTable = updatedIndustryMultipliersTable
+      await Clients.updateOne({ _id: clientId }, { rates: client.rates });
       break;
   }
+  
 };
 
 const replaceOldItem = (arr, replacementItem) => {
   const { _id } = replacementItem;
   const itemToUpdateIndex = findIndexToReplace(arr, _id);
-  return arr.splice(itemToUpdateIndex, 1, replacementItem);
+  arr.splice(itemToUpdateIndex, 1, replacementItem);
+  return arr;
 };
 
-const findIndexToReplace = (arr, searchItemId) => arr.findIndex(item => item._id === searchItemId);
+const findIndexToReplace = (arr, searchItemId) => arr.findIndex(item => item._id.toString() === searchItemId);
 
 module.exports = { updateClientRates };
