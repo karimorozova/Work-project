0270a157874aa15597b4a7a040f09af51c02dc27
PLMR <maxyplmr@gmail.com>
2020-08-14 16:18:21 +0300

    - Fix grammar

--- admin/src/components/vendors/pricelists/LangTable.vue
+++ admin/src/components/vendors/pricelists/LangTable.vue
@@ -137,7 +137,7 @@ export default {
             row: this.dataArray[index]
           })
       } catch (err) {
-        this.alertToggle({message: "Impossibly update price", isShow: true, type: "error" });
+        this.alertToggle({message: "Impossible update price", isShow: true, type: "error" });
       }finally{
         this.refreshResultTable();
         const vendor = await this.$http.get(`/vendorsapi/vendor?id=${this.$route.params.id}`);
