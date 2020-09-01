56be925dc07fb4ef1e1dc49d8aa490589eee7c0e
PLMR <maxyplmr@gmail.com>
2020-09-01 15:36:14 +0300

    - Dashboard Overall View

---
+++ admin/src/components/dashboard/OverallView.vue
@@ -0,0 +1,148 @@
+<template lang="pug">
+.overallView
+  .overallView__col
+    .col__title Today
+    .col__table
+      TableMargin(:tableData="tableMarginToday")
+    .col__chart
+      PieChart(:pieChartTitles="pieChartTitles", :pieChartParams="pieChartToday")
+    .col__table
+      TableClientReceivables(:tableData="tableClientReceivablesToday")
+  .overallView__spaceLine
+    .overallView__spaceLine-line
+
+  .overallView__col
+    .col__title This Month
+    .col__table
+      TableMargin(:tableData="tableMarginMonth")
+    .col__chart
+      PieChart(:pieChartTitles="pieChartTitles", :pieChartParams="pieChartMonth")
+    .col__table
+      TableClientReceivables(:tableData="tableClientReceivablesMonth")
+</template>
+<script>
+import TableMargin from "./OverallViewChildrens/TableMargin";
+import TableClientReceivables from "./OverallViewChildrens/TableClientReceivables";
+import PieChart from "./OverallViewChildrens/PieChart";
+
+export default {
+  data() {
+    return {
+      pieChartTitles: [
+        {
+          title: "Current Clients",
+          color: "#d15f45",
+        },
+        {
+          title: "New Business",
+          color: "#ff8376",
+        },
+      ],
+      tableMarginToday: [
+        {
+          receivables: 123,
+          payable: 123,
+          margin: 123,
+        },
+      ],
+      tableMarginMonth: [
+        {
+          receivables: 234,
+          payable: 234,
+          margin: 234,
+        },
+      ],
+      tableClientReceivablesToday: [
+        {
+          client: "UFX.com",
+          receivables: 123,
+        },
+        {
+          client: "Playtika",
+          receivables: 123,
+        },
+        {
+          client: "Bestmart",
+          receivables: 123,
+        },
+      ],
+      tableClientReceivablesMonth: [
+        {
+          client: "UFX.com",
+          receivables: 345,
+        },
+        {
+          client: "Playtika",
+          receivables: 345,
+        },
+        {
+          client: "Bestmart",
+          receivables: 345,
+        },
+      ],
+      pieChartToday: {
+        chartData: {
+          labels: ["800", "200"],
+          series: [80, 20],
+        },
+        chartOptions: {
+          showLabel: true,
+          width: 250,
+          height: 250,
+        },
+      },
+      pieChartMonth: {
+        chartData: {
+          labels: ["600", "400"],
+          series: [60, 40],
+        },
+        chartOptions: {
+          showLabel: true,
+          width: 250,
+          height: 250,
+        },
+      },
+    };
+  },
+  components: {
+    TableMargin,
+    TableClientReceivables,
+    PieChart,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.overallView {
+  width: 1000px;
+  margin: 40px;
+  display: flex;
+
+  &__spaceLine {
+    width: 100px;
+    display: flex;
+    margin-top: 75px;
+    justify-content: center;
+    &-line {
+      width: 1px;
+      height: auto;
+      background: #dbd3c9;
+    }
+  }
+  &__col {
+    width: 450px;
+  }
+  .col {
+    &__title {
+      text-align: center;
+      font-size: 22px;
+      font-weight: bold;
+    }
+    &__table {
+      margin-top: 50px;
+    }
+    &__chart {
+      margin-top: 50px;
+    }
+  }
+}
+</style>
---
+++ admin/src/components/dashboard/OverallViewChildrens/TableMargin.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+.overallTable
+  SettingsTable(:fields="fields", :tableData="tableData")
+    template(v-for="field in fields", :slot="field.headerKey", slot-scope="{ field }")
+      .overallTable__head-title {{ field.label }}
+    template(slot="receivables", slot-scope="{ row, index }")
+      .overallTable__data {{ row.receivables }} &#8364;
+    template(slot="payable", slot-scope="{ row, index }")
+      .overallTable__data {{ row.payable }} &#8364;
+    template(slot="margin", slot-scope="{ row, index }")
+      .overallTable__data {{ row.margin }} &#8364;
+</template>
+<script>
+import SettingsTable from "../../Table/SettingsTable";
+
+export default {
+  props: {
+    tableData: {
+      type: Array,
+      default: [],
+    },
+  },
+  data() {
+    return {
+      fields: [
+        {
+          label: "Receivables",
+          headerKey: "headerReceivables",
+          key: "receivables",
+          width: "34%",
+          padding: "0",
+        },
+        {
+          label: "Payable",
+          headerKey: "headerPayable",
+          key: "payable",
+          width: "33%",
+          padding: "0",
+        },
+        {
+          label: "Margin",
+          headerKey: "headerMargin",
+          key: "margin",
+          width: "33%",
+          padding: "0",
+        },
+      ],
+    };
+  },
+  components: {
+    SettingsTable,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "../../../assets/scss/colors.scss";
+@import "../../../assets/styles/settingsTable";
+.overallTable {
+  padding: 20px;
+  box-shadow: 0 0 10px #67573e9d;
+  &__data {
+    @extend %table-data;
+    overflow-x: hidden;
+  }
+}
+</style>
