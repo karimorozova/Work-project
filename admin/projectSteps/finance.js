995e042f167e37dbb5b05182ae1979945332a6bd
Miras <peysho@mail.ru>
2020-09-09 17:15:42 +0300

    Changed 'refresh metrics' backend logic

--- admin/projectSteps/finance.js
+++ admin/projectSteps/finance.js
@@ -1,56 +1,59 @@
 function getStepsWithFinanceUpdated(step, project) {
-    let { steps } = project;
-    const task = project.tasks.find(item => item.taskId === step.taskId);
-    //S1
-    const { receivables, payables } = task.service.calculationUnit === 'Words' ? getWordsPrices(step, task.metrics) : getPrices(step);
-    const stepIndex = steps.findIndex(item => item.id === step._id);
-    steps[stepIndex] = {
-        ...step, 
-        finance: {
-            ...step.finance, Price: {receivables, payables}
-        }
-    };
-    return steps;
+  let { steps } = project;
+  const task = project.tasks.find(item => item.taskId === step.taskId);
+  const isForCatWordcount = step.hasOwnProperty('totalWords');
+  const { receivables, payables } = isForCatWordcount ? getWordsPrices(step, task.metrics) : getPrices(step);
+  const stepIndex = steps.findIndex(item => item.id === step._id);
+  steps[stepIndex] = {
+    ...step,
+    finance: {
+      ...step.finance, Price: { receivables, payables }
+    }
+  };
+  return steps;
 }
 
 function getPrices(step) {
-    const { clientRate, vendorRate, finance } = step;
-    let receivables = +finance.Price.receivables;
-    let payables = +finance.Price.payables;
-    if(clientRate) {
-        const clientValue = step.serviceStep.calculationUnit === 'Hours' ? +(step.hours*step.quantity*clientRate.value).toFixed(2) : clientRate.value;
-        receivables = clientValue > clientRate.min ? clientValue : clientRate.min;
-    }
-    if(vendorRate) {
-        const vendorValue = step.serviceStep.calculationUnit === 'Hours' ? +(step.hours*step.quantity*vendorRate.value).toFixed(2) : vendorRate.value;
-        payables = vendorValue > vendorRate.min ? vendorValue : vendorRate.min;
-    }
-    return { receivables, payables }
+  const { clientRate, vendorRate, finance } = step;
+  let receivables = +finance.Price.receivables;
+  let payables = +finance.Price.payables;
+  const isHours = step.hasOwnProperty('hours');
+  if (clientRate) {
+    const clientValue = isHours ? +(step.hours * clientRate.value).toFixed(2) : clientRate.value;
+    receivables = clientValue > clientRate.min ? clientValue : clientRate.min;
+  }
+  if (vendorRate) {
+    const vendorValue = isHours ? +(step.hours * vendorRate.value).toFixed(2) : vendorRate.value;
+    payables = vendorValue > vendorRate.min ? vendorValue : vendorRate.min;
+  }
+  return { receivables, payables }
 }
 
 function getWordsPrices(step, metrics) {
-    const { clientRate, vendorRate } = step;
-    let receivables = metrics.totalWords*clientRate.value
-    let payables = metrics.totalWords*vendorRate.value;
-    let wordsSum = 0;
-    //step.name S2
-    if(step.serviceStep.title === "translation") {
-        for(let key in metrics) {
-            if(key !== 'totalWords') {
-                receivables += +metrics[key].value*metrics[key].client*clientRate.value;
-                payables += +metrics[key].value*metrics[key].vendor*vendorRate.value;
-                wordsSum += metrics[key].value;
-            }
-        }
-        receivables += +(metrics.totalWords - wordsSum)*clientRate.value;
-        payables += +(metrics.totalWords - wordsSum)*vendorRate.value;
+  const { clientRate, vendorRate } = step;
+  let receivables = step.totalWords * clientRate.value
+  const doesStepHasVendorRate = vendorRate.hasOwnProperty('value');
+  let payables = doesStepHasVendorRate ? step.totalWords * vendorRate.value : step.finance.payables;
+  let wordsSum = 0;
+  if (step.name === "Translation") {
+    for (let key in metrics) {
+      if (key !== 'totalWords') {
+        receivables += +metrics[key].value * metrics[key].client * clientRate.value;
+        payables += doesStepHasVendorRate ? +metrics[key].value * metrics[key].vendor * vendorRate.value
+          : +metrics[key].value;
+        wordsSum += metrics[key].value;
+      }
     }
-    receivables = receivables < clientRate.min ? clientRate.min : receivables;
-    payables = payables < vendorRate.min ? vendorRate.min : payables;
-    return { 
-        receivables: parseFloat(receivables.toFixed(2)), 
-        payables: parseFloat(payables.toFixed(2)) 
-    };
+    receivables += +(step.totalWords - wordsSum) * clientRate.value;
+    payables += doesStepHasVendorRate ? +(step.totalWords - wordsSum) * vendorRate.value
+      : +(step.totalWords - wordsSum);
+  }
+  receivables = receivables < clientRate.min ? clientRate.min : receivables;
+  payables = doesStepHasVendorRate ? payables < vendorRate.min ? vendorRate.min : payables : payables;
+  return {
+    receivables: parseFloat(receivables.toFixed(2)),
+    payables: parseFloat(payables.toFixed(2))
+  };
 }
 
 module.exports = { getStepsWithFinanceUpdated }
