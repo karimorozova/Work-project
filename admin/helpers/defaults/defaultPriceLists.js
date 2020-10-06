893f69f23c2996711b26aabcca870214bbf81872
Miras <peysho@mail.ru>
2020-10-06 15:50:20 +0300

    Change default BasicPrices table creation

--- admin/helpers/defaults/defaultPriceLists.js
+++ admin/helpers/defaults/defaultPriceLists.js
@@ -1,39 +1,22 @@
-const { Vendors, Clients, Languages, Units, Industries, CurrencyRatio } = require('../../models');
+const { Languages, Units, Industries, CurrencyRatio } = require('../../models');
 const ObjectId = require('mongodb').ObjectID;
 
 const getDefaultBasicPrices = async () => {
-  const vendors = await Vendors.find({ competencies: { $gt: [] } });
-  const clients = await Clients.find({ sourceLanguages: { $gt: [] }, targetLanguages: { $gt: [] } });
   const currencyRatio = await CurrencyRatio.find();
   const { USD, GBP } = currencyRatio[0];
-  const uniqueDuoLangs = await getUniqueLangPairs(vendors, clients);
   const defaultBasicPrices = [];
-  let sourcesArr = [];
-  let targetsArr = [];
-
-  for (let uniquePair of uniqueDuoLangs) {
-    const splicedString = uniquePair.split(' > ');
-    sourcesArr.push(splicedString[0]);
-    targetsArr.push(splicedString[1]);
-  }
-
   const allLanguages = await Languages.find({});
-  let originallySources = [...new Set(sourcesArr)].map(item => allLanguages.find(i => i.lang === item));
-  let originallyTargets = [...new Set(targetsArr)].map(item => allLanguages.find(i => i.lang === item));
-
-  originallySources.forEach(source => {
-    return originallyTargets.forEach(target => {
-      return defaultBasicPrices.push({
-        type: source.lang === target.lang ? 'Mono' : 'Duo',
-        sourceLanguage: source._id,
-        targetLanguage: target._id,
-        euroBasicPrice: 1,
-        usdBasicPrice: USD,
-        gbpBasicPrice: GBP
-      });
+  const EnglishBritain = allLanguages.find(({ lang }) => lang === 'English (United Kingdom)');
+  allLanguages.forEach(language => {
+    return defaultBasicPrices.push({
+      type: language.lang === EnglishBritain.lang ? 'Mono' : 'Duo',
+      sourceLanguage: EnglishBritain._id,
+      targetLanguage: language._id,
+      euroBasicPrice: 1,
+      usdBasicPrice: USD,
+      gbpBasicPrice: GBP
     });
   });
-
   return defaultBasicPrices;
 };
 
@@ -80,26 +63,6 @@ const getDefaultIndustryMultipliers = async () => {
   return defaultIndustryMultipliers;
 };
 
-const getUniqueLangPairs = async (vendors, clients) => {
-  let langPairs = [];
-  for (let { competencies } of vendors) {
-    for (let { sourceLanguage, targetLanguage } of competencies) {
-      const { lang } = await Languages.findOne({ _id: sourceLanguage });
-      const { lang: targetLang } = await Languages.findOne({ _id: targetLanguage });
-      langPairs.push(`${lang} > ${targetLang}`);
-    }
-  }
-  for (let { sourceLanguages, targetLanguages } of clients) {
-    for (const source of sourceLanguages) {
-      for (const target of targetLanguages) {
-        const { lang } = await Languages.findOne({ _id: source });
-        const { lang: targetLang } = await Languages.findOne({ _id: target });
-        langPairs.push(`${lang} > ${targetLang}`);
-      }
-    }
-  }
-  return Array.from(new Set(langPairs));
-};
 
 
 module.exports = { getDefaultBasicPrices, getDefaultStepMultipliers, getDefaultIndustryMultipliers };
