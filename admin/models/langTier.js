045a2be00ca772f347bbedaef649eecfa49b5dd3
Maxym Palamar <maxyplmr@gmail.com>
2020-10-20 17:44:11 +0300

    - Back Old Model

--- admin/models/langTier.js
+++ admin/models/langTier.js
@@ -1,40 +1,19 @@
 const mongoose = require('mongoose');
 
-// const LangTierSchema = new mongoose.Schema({
-//   languages: {
-//     type : Object,
-//     default: {}
-//   },
-//   industry: {
-//     type : String,
-//     default : 'All',
-//     trim : true
-//   },
-//   updatedAt: {
-//     type : Date,
-//     default : new Date()
-//   },
-// });
-
 const LangTierSchema = new mongoose.Schema({
-	industry: [{
-		name: 'Igaming',
-    
-		source: [{
-			lang: 'AFR',
-			target: [{
-				lang: 'ENG',
-				count: 123
-			}]
-		},
-			{
-				lang: 'ENG',
-				target: [{
-					lang: 'eEEEE',
-					count: 123
-				}]
-			}]
-	}]
+  languages: {
+    type : Object,
+    default: {}
+  },
+  industry: {
+    type : String,
+    default : 'All',
+    trim : true
+  },
+  updatedAt: {
+    type : Date,
+    default : new Date()
+  },
 });
 
 const LangTier = mongoose.model('LangTier', LangTierSchema);
