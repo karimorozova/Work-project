ed65c4b5df8fabf55d3479cb19ec5c7e61826da4
Maxym Palamar <maxyplmr@gmail.com>
2020-11-02 15:10:57 +0200

    - Repair functions for calculate Tier Lang Reports from local projects

--- admin/projects/langTierReport.js
+++ admin/projects/langTierReport.js
@@ -1,47 +1,44 @@
 const { LangTier, Projects, Languages } = require('../models');
-const { langTierIndustries } = require('../enums');
 
 const fillLangTierReportWithLocal = async () => {
-  const finishedProjects = await Projects.find({
-    $and: [
-      { status: 'Closed' }, { 'tasks.service.title': 'Translation' }, { isTest: false }
-    ]
-  }).populate('industry', ['name']);
-  const langTierReports = await LangTier.find();
-  const localLanguages = await Languages.find();
-  for (let { tasks, industry } of finishedProjects) {
-    if (industry.name === langTierIndustries.iGaming) {
-      const iGamingReportIndex = langTierReports.findIndex(({ industry }) => industry === 'iGaming');
-      for (let { finance, sourceLanguage, targetLanguage } of tasks) {
-        const neededReport = langTierReports[iGamingReportIndex];
-        const taskSourceLanguageName = localLanguages.find(({ symbol }) => symbol === sourceLanguage);
-        const taskTargetLanguageName = localLanguages.find(({ symbol }) => symbol === targetLanguage);
-        const sameSourceIndex = neededReport.findIndex(item => {
-          return item.source === taskSourceLanguageName.group;
-        });
-        if (sameSourceIndex !== -1) {
-          const { targets } = neededReport.source[sameSourceIndex];
-          const sameTargetIndex = targets.findIndex(item => {
-            return item.lang === taskTargetLanguageName.group;
-          });
-          if (sameTargetIndex !== -1) {
-            targets[sameTargetIndex].wordcount += finance.Wordcount.receivables;
-          } else {
-            neededReport.source[sameSourceIndex].targets.push({
-              wordcount: finance.Wordcount.receivables,
-              lang: taskTargetLanguageName.group
-            });
-          }
-        } else {
-          neededReport.source.push({
-            clients: 1,
-            lang: taskSourceLanguageName.group,
-            targets: [{ wordcount: finance.Wordcount.receivables, lang: taskTargetLanguageName.group }]
-          });
-        }
-      }
-    }
-  }
+	const date = new Date();
+	const sixMonthsAgo = new Date(date.setMonth(date.getMonth() - 6)).toISOString();
+	let finishedProjects = await Projects.find({ $and: [{ status: 'Closed' }, { isTest: false }, { startDate: { '$gte': sixMonthsAgo } }] })
+			.populate('industry', ['name']);
+	const langTierReports = await LangTier.find();
+	const localLanguages = await Languages.find();
+
+	finishedProjects = finishedProjects.filter(({ industry }) => industry.name === 'iGaming' || industry.name === 'Finance');
+	for (let { steps, industry } of finishedProjects) {
+		const reportIndex = langTierReports.findIndex(i => i.industry === industry.name);
+		const reportAllIndex = langTierReports.findIndex(i => i.industry === 'All');
+		if(reportIndex !== -1) {
+			steps = steps.filter(i => i.name === 'Translation');
+			for (const { sourceLanguage, targetLanguage, finance } of steps) {
+				recalculateWordcount(reportIndex, sourceLanguage, targetLanguage, finance);
+				recalculateWordcount(reportAllIndex, sourceLanguage, targetLanguage, finance);
+
+			}
+		}
+
+		for (let report of langTierReports) await LangTier.updateOne({ _id: report._id }, report);
+
+		function recalculateWordcount(reportIndex, sourceLanguage, targetLanguage, finance) {
+			const source = localLanguages.find(({ symbol }) => symbol === sourceLanguage);
+			const target = localLanguages.find(({ symbol }) => symbol === targetLanguage);
+			const sameSourceIndex = langTierReports[reportIndex].source.findIndex(i => i.lang === source.group);
+			if(sameSourceIndex === -1) {
+				langTierReports[reportIndex].source.push({ lang: source.group, targets: [{ wordcount: finance.Wordcount.receivables, lang: target.group }] })
+			} else {
+				const sameTargetIndex = langTierReports[reportIndex].source[sameSourceIndex].targets.findIndex(i => i.lang === target.group);
+				if(sameTargetIndex === -1) {
+					langTierReports[reportIndex].source[sameSourceIndex].targets.push({ wordcount: finance.Wordcount.receivables, lang: target.group })
+				} else {
+					langTierReports[reportIndex].source[sameSourceIndex].targets[sameTargetIndex].wordcount += finance.Wordcount.receivables;
+				}
+			}
+		}
+	}
 };
 
 module.exports = { fillLangTierReportWithLocal };
