487c8308218033613baaab2c7cf78c70caca58d8
Maxym Palamar <maxyplmr@gmail.com>
2020-11-03 17:36:15 +0200

    - Start functionality for Gmai API

---
+++ admin/models/gmailMessages.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const gmailMessagesSchema = new mongoose.Schema({
+	id: {
+		type: 'String',
+		default: '',
+	},
+	additionalInformation: {}
+
+});
+const GmailMessages = mongoose.model('GmailMessages', gmailMessagesSchema);
+
+module.exports = GmailMessages;
