6163a1e47ee72fb0cc2756e91c55ac69ce82c7a2
Maxym Palamar <maxyplmr@gmail.com>
2020-12-18 17:56:09 +0200

    - Fix problems with delete Vendor Rates

--- admin/vendors/deleteVendorRates.js
+++ admin/vendors/deleteVendorRates.js
@@ -1,31 +1,117 @@
-const { Vendors } = require('../models');
-const { getPricelistCombinations } = require('../clients');
+const { Vendors } = require("../models");
+const { getPricelistCombinations } = require("../clients");
 
-const deleteVendorRates = async (vendorId, competenceToDelete) => {
-  const vendor = await Vendors.findOne({ _id: vendorId });
-  let { basicPricesTable, stepMultipliersTable, industryMultipliersTable, pricelistTable } = vendor.rates;
-  let { sourceLanguage, targetLanguage, step, industry } = competenceToDelete;
-  basicPricesTable = basicPricesTable.filter(item => (
-    `${item.sourceLanguage} ${item.targetLanguage}` !== `${sourceLanguage} ${targetLanguage}`
-  ));
-  stepMultipliersTable = stepMultipliersTable.filter(item => item.step.toString() !== step.toString());
-  industryMultipliersTable = industryMultipliersTable.filter(item => item.industry.toString() !== industry.toString());
-  pricelistTable = await getPricelistCombinations(
-    basicPricesTable,
-    stepMultipliersTable,
-    industryMultipliersTable,
-    pricelistTable,
-    true);
-  return {
-    rates: {
-      basicPricesTable,
-      stepMultipliersTable,
-      industryMultipliersTable,
-      pricelistTable
-    }
-  };
+const deleteVendorRates = async (vendorId, rowToDelete) => {
+	const vendor = await Vendors.findOne({ _id: vendorId });
+	const { rates, competencies } = vendor;
+	let { pricelistTable } = rates;
+	let { sourceLanguage, targetLanguage, step, industry } = rowToDelete;
+	const langPair = `${ sourceLanguage } ${ targetLanguage }`;
+
+	const filteredBasicPricesTable = filterRedundantLangPair(
+			rates,
+			competencies,
+			rowToDelete._id,
+			langPair
+	);
+	const filteredStepsTable = filterRedundantSteps(
+			rates,
+			competencies,
+			rowToDelete._id,
+			step
+	);
+	const filteredIndustriesTable = filterRedundantIndustry(
+			rates,
+			competencies,
+			rowToDelete._id,
+			industry
+	);
+	pricelistTable = getPricelistCombinations(
+			filteredBasicPricesTable,
+			filteredStepsTable,
+			filteredIndustriesTable,
+			pricelistTable,
+			true
+	);
+	return {
+		rates: {
+			basicPricesTable: filteredBasicPricesTable,
+			stepMultipliersTable: filteredStepsTable,
+			industryMultipliersTable: filteredIndustriesTable,
+			pricelistTable
+		}
+	};
+};
+
+const filterRedundantLangPair = (
+		rates,
+		competencies,
+		rowToDeleteId,
+		langPair
+) => {
+	const { basicPricesTable } = rates;
+	const otherCompetencies = competencies.filter(
+			row => row._id.toString() !== rowToDeleteId.toString()
+	);
+
+	const otherLangPairs = otherCompetencies.map(
+			row => `${ row.sourceLanguage } ${ row.targetLanguage }`
+	);
+	const redundantLangPairs = [];
+	if(!otherLangPairs.includes(langPair)) redundantLangPairs.push(langPair);
+	if(redundantLangPairs.length) {
+		return basicPricesTable.filter(
+				({ sourceLanguage, targetLanguage }) =>
+						!redundantLangPairs.includes(`${ sourceLanguage } ${ targetLanguage }`)
+		);
+	}
+	return basicPricesTable;
+};
+
+const filterRedundantSteps = (
+		rates,
+		competencies,
+		rowToDeleteId,
+		stepToDelete
+) => {
+	const { stepMultipliersTable } = rates;
+	const otherCompetencies = competencies.filter(
+			row => row._id.toString() !== rowToDeleteId.toString()
+	);
+	let otherSteps = otherCompetencies.map(row => row.step.toString());
+	const redundantSteps = [];
+	if(!otherSteps.includes(stepToDelete))
+		redundantSteps.push(stepToDelete.toString());
+	if(redundantSteps.length) {
+		return stepMultipliersTable.filter(
+				({ step }) => !redundantSteps.includes(step.toString())
+		);
+	}
+	return stepMultipliersTable;
+};
+
+const filterRedundantIndustry = (
+		rates,
+		competencies,
+		rowToDeleteId,
+		industryToDelete
+) => {
+	const { industryMultipliersTable } = rates;
+	const otherServices = competencies.filter(
+			row => row._id.toString() !== rowToDeleteId.toString()
+	);
+	const otherIndustries = otherServices.map(row => row.industry.toString());
+	const redundantIndustries = [];
+	if(!otherIndustries.includes(industryToDelete))
+		redundantIndustries.push(industryToDelete.toString());
+	if(redundantIndustries.length) {
+		return industryMultipliersTable.filter(
+				({ industry }) => !redundantIndustries.includes(industry.toString())
+		);
+	}
+	return industryMultipliersTable;
 };
 
 module.exports = {
-  deleteVendorRates
+	deleteVendorRates
 };
