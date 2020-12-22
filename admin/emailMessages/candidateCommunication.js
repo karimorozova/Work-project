4db2c6ebba4c56ee9668c1a878332ed22fe0350b
Maxym Palamar <maxyplmr@gmail.com>
2020-12-22 13:01:16 +0200

    - Final Test candidate communications

--- admin/emailMessages/candidateCommunication.js
+++ admin/emailMessages/candidateCommunication.js
@@ -1,28 +1,28 @@
 function testSentMessage(obj) {
-    let date = new Date();
-    date.setDate(date.getDate() + 5);
-    const sourceLang = obj.source ? obj.source.lang : "NA";
-    const industriesList = obj.industries.reduce((acc, curr) => acc + `<li>${curr.name};</li>`, '');
-    return `<div class="wrapper" style="width:800px;border-width:1px;border-style:solid;border-color:rgb(129, 129, 129);font-family:'Roboto', sans-serif;color:#66563E;box-sizing:border-box;" >
+	let date = new Date();
+	date.setDate(date.getDate() + 5);
+	const sourceLang = obj.source ? obj.source.lang : "NA";
+	const industriesList = obj.industries.reduce((acc, curr) => acc + `<li>${ curr.name };</li>`, '');
+	return `<div class="wrapper" style="width:800px;border-width:1px;border-style:solid;border-color:rgb(129, 129, 129);font-family:'Roboto', sans-serif;color:#66563E;box-sizing:border-box;" >
                 <header style="background-color:#66563E;text-align:center;" >
                     <img class="logo" src="cid:logo@pan" alt="pangea" style="margin-top:20px;margin-bottom:20px;margin-right:0;margin-left:0;" >
                 </header>
                 <div class="main" style="padding-top:40px;padding-bottom:40px;padding-right:40px;padding-left:40px;" >
-                    <h4 class="contact-name">Dear ${obj.firstName}</h4>
-                    <p>
-                        As part of our initial process, please find attached our sample text in: ${sourceLang}.
-                        The sample translation focuses on terminology(s):
-                        <ul>
-                          ${industriesList}
+                    <p style="background: #F4F0EE; font-size: 14px; font-weight: bold; padding: 14px;"><span id="client-name-row">Dear ${ obj.firstName }</span></p>
+                    <p style="font-weight: 400;">
+                        As part of our initial process, please find attached our sample text in: ${ sourceLang }. <br>
+                        The sample translation focuses on terminology(s): <br>
+                        <ul style="font-weight: 400;">
+                          ${ industriesList }
                         </ul>
-                        Please translate the sample into: ${obj.target.lang} and keep in mind that the translation has to be delivered by ${date}.
-                        Kindly confirm you have received this email and please advise ahead if you have any issues with the deadline that was provided.
+                        <span style="font-weight: 400;">
+	                        Please translate the sample into: ${ obj.target.lang } and keep in mind that the translation has to be delivered by ${ date }. <br>
+	                        Kindly confirm you have received this email and please advise ahead if you have any issues with the deadline that was provided.
+                        </span>
                     </p>
-                    <p>
-                        If you have any question, please do not hesitate to contact us.
-
-                        Many thanks,
-                        
+                    <p style="font-weight: 400;">
+                        If you have any question, please do not hesitate to contact us. <br>
+                        Many thanks,<br>
                         Pangea HR team
                     </p>
                 </div>
@@ -34,23 +34,20 @@ function testSentMessage(obj) {
 }
 
 function testNotPassedMessage(obj) {
-    return `<div class="wrapper" style="width:800px;border-width:1px;border-style:solid;border-color:rgb(129, 129, 129);font-family:'Roboto', sans-serif;color:#66563E;box-sizing:border-box;" >
+	return `<div class="wrapper" style="width:800px;border-width:1px;border-style:solid;border-color:rgb(129, 129, 129);font-family:'Roboto', sans-serif;color:#66563E;box-sizing:border-box;" >
                 <header style="background-color:#66563E;text-align:center;" >
                     <img class="logo" src="cid:logo@pan" alt="pangea" style="margin-top:20px;margin-bottom:20px;margin-right:0;margin-left:0;" >
                 </header>
                 <div class="main" style="padding-top:40px;padding-bottom:40px;padding-right:40px;padding-left:40px;" >
-                    <h4 class="contact-name">Hello ${obj.firstName}</h4>
-                    <p>
-                        Thank you again for applying for the ${obj.target.lang}-Translator@Pangea position.
+                    <p style="background: #F4F0EE; font-size: 14px; font-weight: bold; padding: 14px;"><span id="client-name-row">Hello ${ obj.firstName }</span></p>
+                    <p style="font-weight: 400;" > 
+                        Thank you again for applying for the ${ obj.target.lang }-Translator@Pangea position. <br>
                         Your test and qualifications have been reviewed carefully and although your talent is valued, we have decided to pursue other candidates at this stage.
                     </p>
-                    <p>
-                        Should something change on our side, we will not hesitate to contact you.
-
-                        Thank you again for your time and effort, and we wish you the best in your current endeavors.
-                        
-                        All the best,
-                        
+                    <p style="font-weight: 400;" >
+                        Should something change on our side, we will not hesitate to contact you. <br>
+                        Thank you again for your time and effort, and we wish you the best in your current endeavors. <br>
+                        All the best, <br>
                         Pangea HR Team
                     </p>
                 </div>
@@ -62,18 +59,18 @@ function testNotPassedMessage(obj) {
 }
 
 function testPassedMessage(obj) {
-    return `<div class="wrapper" style="width:800px;border-width:1px;border-style:solid;border-color:rgb(129, 129, 129);font-family:'Roboto', sans-serif;color:#66563E;box-sizing:border-box;" >
+	return `<div class="wrapper" style="width:800px;border-width:1px;border-style:solid;border-color:rgb(129, 129, 129);font-family:'Roboto', sans-serif;color:#66563E;box-sizing:border-box;" >
                 <header style="background-color:#66563E;text-align:center;" >
                     <img class="logo" src="cid:logo@pan" alt="pangea" style="margin-top:20px;margin-bottom:20px;margin-right:0;margin-left:0;" >
                 </header>
                 <div class="main" style="padding-top:40px;padding-bottom:40px;padding-right:40px;padding-left:40px;" >
-                    <h4 class="contact-name">Hello ${obj.firstName}</h4>
-                    <p>
-                        Your test was evaluated and received positive feedback
-                        Please review the test results attached.
+                    <p style="background: #F4F0EE; font-size: 14px; font-weight: bold; padding: 14px;"><span id="client-name-row">Hello ${ obj.firstName }</span></p>
+                    <p style="font-weight: 400;">
+                        Your test was evaluated and received positive feedback <br>
+                        Please review the test results attached. <br>
                         If you have any comments regarding evaluation, let us know.
                     </p>
-                    <p>
+                    <p style="font-weight: 400;">
                         You will receive shortly email with our offer.
                     </p>
                 </div>
@@ -85,7 +82,7 @@ function testPassedMessage(obj) {
 }
 
 module.exports = {
-    testSentMessage,
-    testNotPassedMessage,
-    testPassedMessage
+	testSentMessage,
+	testNotPassedMessage,
+	testPassedMessage
 }
