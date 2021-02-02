cc042e4d8ffc94dcbf53006b74ab9a67ea24c5d6
Maxym Palamar <maxyplmr@gmail.com>
2021-02-02 16:10:09 +0200

    - Language setting

--- admin/routes/settings.js
+++ admin/routes/settings.js
@@ -2,8 +2,9 @@ const router = require('express').Router()
 const fs = require('fs')
 const { upload } = require('../utils/')
 const { updateLanguage } = require('../settings')
+const { Languages } = require('../models')
 
-router.post('/languages', upload.fields([{ name: "flag" }]), async (req, res) => {
+router.post('/languages', upload.fields([ { name: "flag" } ]), async (req, res) => {
 	const flag = req.files["flag"]
 	try {
 		await updateLanguage({ ...req.body, flag })
@@ -11,6 +12,17 @@ router.post('/languages', upload.fields([{ name: "flag" }]), async (req, res) =>
 	} catch (err) {
 		console.log(err)
 		res.status(500).send('Something went wrong while Language saving')
+	}
+})
+
+router.put('/language-setting', async (req, res) => {
+	let { obj } = req.body
+	try {
+		await Languages.updateOne({ _id: obj._id }, obj)
+		res.send('Updated')
+	} catch (err) {
+		console.log(err)
+		res.status(500).send('Error on language setting saving')
 	}
 })
 
