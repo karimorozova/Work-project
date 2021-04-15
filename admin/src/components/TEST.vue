5f2e68f950159b26a790d37a9f71ee04c6a102e5
Dmitrii <dmitrii@pangea.global>
2021-04-15 17:18:45 +0300

    test google auth

--- admin/src/components/TEST.vue
+++ admin/src/components/TEST.vue
@@ -4,7 +4,7 @@
     <p>
       SING IN
     </p>
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    <div id="google-signin-button"></div>
 
     <br>
     <a href="#" @click="signOut">Sign out</a>
@@ -39,27 +39,33 @@
         const el = document.getElementsByTagName('h4')
         el.innerHTML = "ID: " + profile.getId() + 'Full Name: ' + profile.getName() + 'Given Name: ' + profile.getGivenName() + 'Family Name: ' + profile.getFamilyName() + "Image URL: " + profile.getImageUrl() + "Email: " + profile.getEmail()
 			},
-			foo(){
-				console.log('foo')
-				var auth2 = gapi.auth2.getAuthInstance()
-				if (auth2.isSignedIn.get()) {
-					var profile = auth2.currentUser.get().getBasicProfile();
-					console.log('full', profile)
-					console.log('ID: ' + profile.getId());
-					console.log('Full Name: ' + profile.getName());
-					console.log('Given Name: ' + profile.getGivenName());
-					console.log('Family Name: ' + profile.getFamilyName());
-					console.log('Image URL: ' + profile.getImageUrl());
-					console.log('Email: ' + profile.getEmail());
-				}
-      },
+			// foo(){
+			// 	console.log('foo')
+			// 	var auth2 = gapi.auth2.getAuthInstance()
+			// 	if (auth2.isSignedIn.get()) {
+			// 		var profile = auth2.currentUser.get().getBasicProfile();
+			// 		console.log('full', profile)
+			// 		console.log('ID: ' + profile.getId());
+			// 		console.log('Full Name: ' + profile.getName());
+			// 		console.log('Given Name: ' + profile.getGivenName());
+			// 		console.log('Family Name: ' + profile.getFamilyName());
+			// 		console.log('Image URL: ' + profile.getImageUrl());
+			// 		console.log('Email: ' + profile.getEmail());
+			// 	}
+      // },
 			signOut() {
 				var auth2 = gapi.auth2.getAuthInstance()
 				auth2.signOut().then(function () {
 					console.log('User signed out.')
 				})
 			}
+
 		},
+    mounted() {
+      gapi.signin2.render('google-signin-button', {
+        onsuccess: this.onSignIn
+      })
+    },
 		created() {
 			const scripts = [
 				"https://apis.google.com/js/platform.js"
