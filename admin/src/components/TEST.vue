a26ac4ed578e20528131c6fc7e14a8eeb05793f2
Maxym Palamar <maxyplmr@gmail.com>
2021-04-15 10:25:05 +0300

    Update TEST.vue

--- admin/src/components/TEST.vue
+++ admin/src/components/TEST.vue
@@ -4,10 +4,20 @@
     <p>
       SING IN
     </p>
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    <div class="g-signin2" @click="foo" data-onsuccess="onSignIn"></div>
 
-    <script>
-			function onSignIn(googleUser) {
+    <br>
+    <a href="#" @click="signOut">Sign out</a>
+
+  </div>
+</template>
+
+<script>
+
+	export default {
+		methods: {
+			onSignIn(googleUser) {
+				console.log('googleUser', googleUser)
 				// Useful data for your client-side scripts:
 				var profile = googleUser.getBasicProfile()
 				console.log(profile)
@@ -21,18 +31,20 @@
 				// The ID token you need to pass to your backend:
 				var id_token = googleUser.getAuthResponse().id_token
 				console.log("ID Token: " + id_token)
-			}
-    </script>
-    <br>
-    <a href="#" @click="signOut">Sign out</a>
-
-  </div>
-</template>
-
-<script>
-
-	export default {
-		methods: {
+			},
+			foo(){
+				console.log('foo')
+				var auth2 = gapi.auth2.getAuthInstance()
+				if (auth2.isSignedIn.get()) {
+					var profile = auth2.currentUser.get().getBasicProfile();
+					console.log('ID: ' + profile.getId());
+					console.log('Full Name: ' + profile.getName());
+					console.log('Given Name: ' + profile.getGivenName());
+					console.log('Family Name: ' + profile.getFamilyName());
+					console.log('Image URL: ' + profile.getImageUrl());
+					console.log('Email: ' + profile.getEmail());
+				}
+      },
 			signOut() {
 				var auth2 = gapi.auth2.getAuthInstance()
 				auth2.signOut().then(function () {
