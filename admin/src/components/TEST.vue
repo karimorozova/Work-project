591b8a2cf69a9c1f7864c2690ac0f0effaa2f440
Dmitrii <dmitrii@pangea.global>
2021-04-16 11:51:39 +0300

    google auth with backend

--- admin/src/components/TEST.vue
+++ admin/src/components/TEST.vue
@@ -8,7 +8,7 @@
     <br>
     <a href="#" @click="signOut">Sign out</a>
     <br>
-    <div @click="foo"> gET PROFILE INFO</div>
+    <div @click="onSignIn"> sign in with google</div>
     <h4></h4>
 
 
@@ -17,48 +17,66 @@
 
 <script>
 
-	export default {
+	import {mapActions} from "vuex";
+
+  export default {
 		methods: {
-			onSignIn(googleUser) {
-				console.log('googleUser', googleUser)
-				// Useful data for your client-side scripts:
-				var profile = googleUser.getBasicProfile()
-				console.log(profile)
-				console.log("ID: " + profile.getId()) // Don't send this directly to your server!
-				console.log('Full Name: ' + profile.getName())
-				console.log('Given Name: ' + profile.getGivenName())
-				console.log('Family Name: ' + profile.getFamilyName())
-				console.log("Image URL: " + profile.getImageUrl())
-				console.log("Email: " + profile.getEmail())
 
-				// The ID token you need to pass to your backend:
-				var id_token = googleUser.getAuthResponse().id_token
-				console.log("ID Token: " + id_token)
+      ...mapActions({
+        alertToggle: "alertToggle",
+        loggingIn: "login",
+        loggingOut: "logout"
+      }),
+      async onSignIn() {
+        try {
+          const googleUser = await this.$gAuth.signIn();
+          if (!googleUser) {
+            return null;
+          }
+          console.log("googleUser", googleUser);
+          console.log("getId", googleUser.getId());
+          console.log("getBasicProfile", googleUser.getBasicProfile());
+          console.log("getAuthResponse", googleUser.getAuthResponse());
+          console.log(
+            "getAuthResponse",
+            this.$gAuth.GoogleAuth.currentUser.get().getAuthResponse()
+          );
 
-        // const el = document.getElementsByTagName('h4')
-        // el.innerHTML = "ID: " + profile.getId() + 'Full Name: ' + profile.getName() + 'Given Name: ' + profile.getGivenName() + 'Family Name: ' + profile.getFamilyName() + "Image URL: " + profile.getImageUrl() + "Email: " + profile.getEmail()
-			},
-			// foo(){
-      //   this.$gAuth.signin2.render('google-signin-button', {
-      //     onsuccess: this.onSignIn
-      //   })
-      // },
-			signOut() {
-				var auth2 = this.$gAuth.auth2.getAuthInstance()
-				auth2.signOut().then(function () {
-					console.log('User signed out.')
-				})
-			}
+
+          this.isAllFieldsError = false;
+          const loginResult = await this.$http.post('/login-with-google', {idToken: googleUser.getAuthResponse().id_token});
+          await this.loggingIn(loginResult.body);
+          this.alertToggle({ message: "You are logged in", isShow: true, type: "success" });
+          this.$router.push("/")
+
+          this.isSignIn = this.$gAuth.isAuthorized;
+        } catch (error) {
+          //on fail do something
+          console.error(error);
+          return null;
+        }
+      },
+      async signOut() {
+        try {
+          await this.$gAuth.signOut();
+          this.isSignIn = this.$gAuth.isAuthorized;
+          console.log("isSignIn", this.$gAuth.isAuthorized);
+        } catch (error) {
+          console.error(error);
+        }
+      },
+
 
 		},
-    mounted() {
-      this.$gAuth.signin2.render('google-signin-button', {
-        onsuccess: this.onSignIn
-      })
-    },
-		created() {
-		}
-	}
+    created() {
+      let that = this;
+      let checkGauthLoad = setInterval(function () {
+        that.isInit = that.$gAuth.isInit;
+        that.isSignIn = that.$gAuth.isAuthorized;
+        if (that.isInit) clearInterval(checkGauthLoad);
+      }, 1000);
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
