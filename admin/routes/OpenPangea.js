0c4f5871016194a297d01f44408cc6251ccb5c94
Dmitrii <dmitrii@pangea.global>
2021-05-19 12:18:40 +0300

    - Change field name at response client api

--- admin/routes/OpenPangea.js
+++ admin/routes/OpenPangea.js
@@ -5,9 +5,9 @@ router.get('/api-clients', async (req,res)=> {
 	try {
 		let clientsApi = JSON.parse(JSON.stringify(await getClientsApi()))
 		res.send( clientsApi.map(({ affiliation, logo, industry, _id, __v, ...rest}) =>
-      ({ ...rest, logo: 'https://admin.pangea.global' + logo ,industry: industry.map(({name}) => name)}) ))
+      ({ ...rest, logo: 'https://admin.pangea.global' + logo , industries: industry.map(({name}) => name)}) ))
 	} catch (e) {
-		res.status(500).send('Error on client delete')
+		res.status(500).send('Error on get api client')
 	}
 
 })
