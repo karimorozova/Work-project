80ff6409a2f589bcddd1998b745c4e8dd1aae10e
Maxym Palamar <maxyplmr@gmail.com>
2021-09-20 11:22:15 +0300

    Update DeliveryTable.vue

--- portal/components/DeliveryTable.vue
+++ portal/components/DeliveryTable.vue
@@ -76,6 +76,7 @@ export default {
       return tasksDeliverables.map(item => {
       	const allDR2 = [...singleLang, ...multiLang ]
 	        const currentDelivery = allDR2.find(({_id}) => `${_id}` === `${item.deliverablesId}`)
+          if(!currentDelivery) return false
           const languagePair = currentDelivery.hasOwnProperty('sourceLanguage') ?
               `${this.allLanguages.find(({_id}) => `${_id}` === `${currentDelivery.sourceLanguage}`).symbol} >> ${this.allLanguages.find(({_id}) => `${_id}` === `${currentDelivery.targetLanguage}`).symbol}` :
               'Multilingual'
@@ -88,7 +89,7 @@ export default {
 		        languagePair,
 		        filesLength
           }
-      })
+      }).filter(Boolean)
 		}
 	},
 	components: {
