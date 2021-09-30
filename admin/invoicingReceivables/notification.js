22cf7f185c3a74461c075454bfe31316a24d653b
Maxym Palamar <maxyplmr@gmail.com>
2021-09-30 18:26:45 +0300

    - fix

--- admin/invoicingReceivables/notification.js
+++ admin/invoicingReceivables/notification.js
@@ -1,24 +1,25 @@
 const { sendEmail } = require('../utils/mailTemplate')
 const { getReportById } = require('./getReceivables')
-const { generateReportPPP } = require('./statisticReportsGeneration')
+// const { generateReportPPP } = require('./statisticReportsGeneration')
 
 const sendInvoiceToClientContacts = async (_reportId) => {
 	const subject = 'INVOICE READY PLS CHECK'
 	const attachments = []
+	// const reportFiles = []
 	const [ report ] = await getReportById(_reportId)
-	const { client, clientBillingInfo, total, reportId, lastPaymentDate, invoice, reportFiles } = report
+	const { client, clientBillingInfo, total, reportId, lastPaymentDate, invoice } = report
 	const { officialName, contacts, paymentType } = client.billingInfo.find(({ _id }) => `${ _id }` === `${ clientBillingInfo }`)
 
-	if(paymentType === 'PPP'){
-		await generateReportPPP(_reportId)
-	}
-	console.log(reportFiles)
-	if(reportFiles.length) for(let file of reportFiles) attachments.push({ ...file })
+	// if(paymentType === 'PPP'){
+	// 	reportFiles.push( await generateReportPPP(_reportId))
+	// }
+	// console.log(reportFiles)
+	// if(reportFiles.length) for(let file of reportFiles) attachments.push({ ...file })
 	attachments.push({ filename: invoice.filename, path: invoice.path })
 	const finalAttachments = attachments.map(item => ({ filename: item.filename.split('-').pop(), path: `./dist/${ item.path }` }))
 
 	//--------- TODO удалить н=>
-	for await (let contact of [ { email: 'maksym@pangea.global' } ]) {
+	for await (let contact of [ { email: 'maksym@pangea.global' }, {email: 'dmitrii@pangea.global'} ]) {
 		const message = 'INVOICE READY'
 		await sendEmail({ to: contact.email, attachments: finalAttachments, subject }, message)
 	}
