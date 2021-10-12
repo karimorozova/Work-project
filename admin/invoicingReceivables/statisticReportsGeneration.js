893c47ac84f39bdff3b98e28815af68d227b7b26
Dmitrii <dmitrii@pangea.global>
2021-10-12 10:19:26 +0300

    - small fixes notification.js

--- admin/invoicingReceivables/statisticReportsGeneration.js
+++ admin/invoicingReceivables/statisticReportsGeneration.js
@@ -7,6 +7,8 @@ const generateReportPPP = async (_reportId, report, BI) => {
 	const template = await pdfPPPReportTemplate(report, BI)
 	const path = await generateReceivablesReportsByTemplate(_reportId, filename, template)
 	await InvoicingReceivables.updateOne({ _id: _reportId }, { $push: { reportFiles: { filename: filename + '.pdf', path: path.split('./dist/').pop() } } })
+
+	return { filename: filename + '.pdf', path: path.split('./dist/').pop() }
 }
 
 module.exports = {
