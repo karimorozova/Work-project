43dcaadfbf2d4c105a8bd60281cd0bbceaf88841
Maxym Palamar <maxyplmr@gmail.com>
2021-12-08 09:54:12 +0200

    - instructions

--- admin/enums/serviceInstructions.js
+++ admin/enums/serviceInstructions.js
@@ -1,11 +1,11 @@
-module.exports =  {
+module.exports = {
 	"Translation": [
 		{
 			id: '1',
 			title: 'Terms to keep in English:',
 			description:
 					'<ul>' +
-					'<li>list out the terms that must be kept in English</li>' +
+					'<li>List out the terms that must be kept in English</li>' +
 					'</ul>'
 		},
 		{
@@ -13,47 +13,49 @@ module.exports =  {
 			title: 'Terms to translate as follows:',
 			description:
 					'<ul>' +
-					'<li>Please write here any glossary that you wish us to keep as you specify.</li>' +
+					'<li>Please provide any glossary that you wish us to keep as you specify.</li>' +
 					'</ul>'
 		},
 		{
 			id: '3',
-			title: 'Terms to translate as follows:',
+			title: 'Highlighted Text:',
 			description:
 					'<ul>' +
-					'<li>Please write here any glossary that you wish us to keep as you specify.</li>' +
+					'<li>Please provide any glossary that you wish us to keep as you specify.</li>' +
 					'</ul>'
 		},
 		{
 			id: '4',
-			title: 'In case  there is some text highlighted in color - shall it be translated?',
+			title: 'Character limitation requirements:',
 			description:
-					''
+					'<ul>' +
+					'<li>Please specify next to each sentence and also here.</li>' +
+					'</ul>'
 		},
 		{
 			id: '5',
-			title: 'Do you have any character limitation requirements? If yes - please specify next to each sentence.',
+			title: 'Screenshots/images:',
 			description:
-					''
+					'<ul>' +
+					'<li>Need to be translated? Yes/No</li>' +
+					'</ul>'
 		},
 		{
 			id: '6',
-			title: 'If there are screenshots/images with text - do they need to be translated?',
+			title: 'Need to be translated? Yes/No',
 			description:
-					''
+					'<ul>' +
+					'<li>Need to be translated? Yes/No/li>' +
+					'</ul>'
 		},
 		{
 			id: '7',
-			title: 'Please provide reference links or screenshots, that will help our linguists to understand where those content will appear',
+			title: 'Localization:',
 			description:
-					''
-		},
-		{
-			id: '8',
-			title: 'Shall we localize time, dates, numbers, currencies?',
-			description:
-					''
-		},
+					'<ul>' +
+					'<li>Shall we localize time, dates, numbers, currencies?</li>' +
+					'</ul>'
+		}
 	],
 	"Compliance": [
 		{
@@ -66,7 +68,7 @@ module.exports =  {
 					'<li>Issue date</li>' +
 					'<li>Expiry date if there is any</li>' +
 					'</ul>'
-		},
+		}
 	],
 	"ComplianceEtoro": [
 		{
@@ -194,6 +196,343 @@ module.exports =  {
 					'<li>Taxes due from company</li>' +
 					'<li>References to distributions of dividends or profits to shareholders</li>' +
 					'</ul>'
+		}
+	],
+	"Copywriting": [
+		{
+			id: '1',
+			title: 'Content-type',
+			description:
+					'<ul>' +
+					'<li>Article/Blog/Email/Newsletter/SMS/Social Media post</li>' +
+					'</ul>'
+		},
+		{
+			id: '2',
+			title: 'Tone of voice',
+			description:
+					'<ul>' +
+					'<li>formal/informal</li>' +
+					'</ul>'
+		},
+		{
+			id: '3',
+			title: 'Keywords (terms) to include',
+			description: ''
+		},
+		{
+			id: '4',
+			title: 'Reference links',
+			description:
+					'<ul>' +
+					'<li>(websites, images) to similar projects made previously</li>' +
+					'</ul>'
+		},
+		{
+			id: '5',
+			title: 'Overview',
+			description:
+					'<ul>' +
+					'<li>(Explain the concept behind the task. How did it come about? What is the main idea that needs to be conveyed in the creative?)</li>' +
+					'</ul>'
+		},
+		{
+			id: '6',
+			title: 'Objectives',
+			description:
+					'<ul>' +
+					'<li>(What do we want to achieve with this campaign?)</li>' +
+					'</ul>'
+		},
+		{
+			id: '7',
+			title: 'Target audience',
+			description:
+					'<ul>' +
+					'<li>(Who are the primary audience, secondary audience and stakeholders? (depositors, leads, clients)</li>' +
+					'</ul>'
+		},
+		{
+			id: '8',
+			title: 'Promotions',
+			description:
+					'<ul>' +
+					'<li>(What are you offering in this promotion? What type of bonus? What kind of offer?)</li>' +
+					'</ul>'
+		},
+		{
+			id: '9',
+			title: 'Special instructions',
+			description:
+					'<ul>' +
+					'<li>(Are there any special requests needed for this task?)</li>' +
+					'</ul>'
+		},
+		{
+			id: '10',
+			title: 'Wordcount',
+			description: ''
+		}
+	],
+	"Certified Translation": [
+		{
+			id: '1',
+			title: 'Certified Translation',
+			description:
+					'<ul>' +
+					'<li>Certified Translation: You need a hard copy of the translated document and stamped certificate from the linguist to use for legal purposes (Original documents will arrive at our Limassol office).</li>' +
+					'</ul>'
+		},
+		{
+			id: '2',
+			title: 'Official Translation',
+			description:
+					'<ul>' +
+					'<li>Certified Translation: You need a hard copy of the translated document and stamped certificate from the linguist to use for legal purposes (Original documents will arrive at our Limassol office).</li>' +
+					'</ul>'
+		}
+	],
+	"ICR-A": [
+		{
+			id: '1',
+			title: 'Required to provide',
+			description:
+					'<ul>' +
+					'<li>Reference link of the website page(s) &  URLs that need to be QA\'d.</li>' +
+					'</ul>'
+		}
+	],
+	"ICR-B": [
+		{
+			id: '1',
+			title: 'Required to provide',
+			description:
+					'<ul>' +
+					'<li>Source Language Screenshots & Target Language Screenshots</li>' +
+					'</ul>'
+		}
+	],
+	"Proofing": [
+		{
+			id: '1',
+			title: 'Required to provide',
+			description:
+					'<ul>' +
+					'<li>Source Language text & Target Language text</li>' +
+					'</ul>'
+		},
+		{
+			id: '2',
+			title: 'Track changes',
+			description:
+					'<ul>' +
+					'<li>Do you want to have final file with tracked changes? Yes/No</li>' +
+					'</ul>'
+		}
+	],
+	"Voice Over": [
+		{
+			id: '1',
+			title: 'General information about the project',
+			description: ''
+		},
+		{
+			id: '2',
+			title: 'Amount',
+			description:
+					'<ul>' +
+					'<li>Wordcount</li>' +
+					'<li>Minute</li>' +
+					'</ul>'
+		},
+		{
+			id: '3',
+			title: 'Dialect/accent',
+			description: ''
+		},
+		{
+			id: '4',
+			title: 'Male or female voice?',
+			description: ''
+		},
+		{
+			id: '5',
+			title: 'Purpose ',
+			description:
+					'<ul>' +
+					'<li>video narration</li>' +
+					'<li>radio</li>' +
+					'<li>dubbing/impersonation</li>' +
+					'<li>phone system/IVR</li>' +
+					'<li>podcast</li>' +
+					'<li>podcast</li>' +
+					'<li>TV</li>' +
+					'<li>Video Game</li>' +
+					'<li>e-Learning</li>' +
+					'</ul>'
+		},
+		{
+			id: '6',
+			title: 'Target age range',
+			description:
+					'<ul>' +
+					'<li>youth</li>' +
+					'<li>adults</li>' +
+					'<li>middle aged</li>' +
+					'<li>senior</li>' +
+					'</ul>'
+		},
+		{
+			id: '7',
+			title: 'Services included',
+			description:
+					'<ul>' +
+					'<li>HQ Audio File (WAAV format)</li>' +
+					'<li>Full Broadcast Rights</li>' +
+					'<li>Commercial Rights</li>' +
+					'<li>Split Files (up to 5 files)</li>' +
+					'<li>Background Music (per each track)</li>' +
+					'<li>Time Audio Sync (per 60 seconds)</li>' +
+					'<li>Script proofreading</li>' +
+					'<li>Additional revision</li>' +
+					'<li>Extra Fast 1 Day Delivery</li>' +
+					'<li>Add silence (pauses between each line)</li>' +
+					'</ul>'
+		},
+		{
+			id: '8',
+			title: 'Budget (per word/minute)',
+			description: ''
+		}
+	],
+	"TransCreation": [
+		{
+			id: '1',
+			title: 'Terms to keep in English',
+			description:
+					'<ul>' +
+					'<li>List out the terms that must be kept in English</li>' +
+					'</ul>'
+		},
+		{
+			id: '2',
+			title: 'Terms to translate as follows',
+			description:
+					'<ul>' +
+					'<li>Please provide any glossary that you wish us to keep as you specify.</li>' +
+					'</ul>'
+		},
+		{
+			id: '3',
+			title: 'Highlighted Text',
+			description:
+					'<ul>' +
+					'<li>Please specify if a different action should be taken like: ignore and don`t translate</li>' +
+					'</ul>'
+		},
+		{
+			id: '4',
+			title: 'Character limitation requirements',
+			description:
+					'<ul>' +
+					'<li>Please specify next to each sentence and also here</li>' +
+					'</ul>'
+		},
+		{
+			id: '5',
+			title: 'Screenshots/images',
+			description:
+					'<ul>' +
+					'<li>Need to be translated? Yes/No</li>' +
+					'</ul>'
+		},
+		{
+			id: '6',
+			title: 'Reference links/screenshots',
+			description:
+					'<ul>' +
+					'<li>Help the linguists to understand the context, layout and style preferences</li>' +
+					'</ul>'
+		},
+		{
+			id: '7',
+			title: 'Localization',
+			description:
+					'<ul>' +
+					'<li>Shall we localize time, dates, numbers, currencies?</li>' +
+					'</ul>'
+		},
+		{
+			id: '8',
+			title: 'Tone of voice',
+			description:
+					'<ul>' +
+					'<li>formal/informal?</li>' +
+					'</ul>'
+		},
+		{
+			id: '9',
+			title: 'Keywords (terms) to include',
+			description: ''
+		},
+		{
+			id: '10',
+			title: 'Target audience',
+			description:
+					'<ul>' +
+					'<li>(Who are the primary audience, secondary audience and stakeholders? (depositors, leads, clients)</li>' +
+					'</ul>'
+		},
+		{
+			id: '11',
+			title: 'This content will be used on',
+			description: ''
+		}
+	],
+	"Transcription": [
+		{
+			id: '1',
+			title: 'Type of recording',
+			description:
+					'<ul>' +
+					'<li>(pone call, audio recording of the speech etc.)</li>' +
+					'</ul>'
+		},
+		{
+			id: '2',
+			title: 'How many people are talking',
+			description:
+					'<ul>' +
+					'<li>Specify the number</li>' +
+					'</ul>'
+		},
+		{
+			id: '3',
+			title: 'Amount of minutes',
+			description: ''
+		}
+	],
+	"Copywriting": [
+		{
+			id: '1',
+			title: 'Source',
+			description:
+					'<ul>' +
+					'<li>Script or audio</li>' +
+					'</ul>'
+		},
+		{
+			id: '2',
+			title: 'Embed Subtitles',
+			description:
+					'<ul>' +
+					'<li>Yes/No</li>' +
+					'</ul>'
+		},
+		{
+			id: '3',
+			title: 'Which format to receive the final work',
+			description: ''
 		}
 	]
 }
