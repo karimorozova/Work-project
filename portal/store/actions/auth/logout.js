e1efc7b477ac778d02c28cb6650c352a44a389df
Maxym Palamar <maxyplmr@gmail.com>
2021-12-16 09:18:56 +0200

    Update logout.js

--- portal/store/actions/auth/logout.js
+++ portal/store/actions/auth/logout.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
+import Vue from "vue"
 
-export const logout =  ({ commit }) => {
-    commit("SET_TOKEN", "");
-    commit("SET_PREVIOUS_LINK", "");
-    // Vue.cookie.delete("client");
+export const logout = ({ commit }) => {
+	commit("SET_TOKEN", "")
+	commit("SET_PREVIOUS_LINK", "")
 
-    //force deletion cookie
-    let cookies = document.cookie.split(";")
-    for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i]
-        let eqPos = cookie.indexOf("=")
-        let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
-    }
-};
+	//force deletion cookie
+	let cookies = document.cookie.split(";")
+	for (let i = 0; i < cookies.length; i++) {
+		const [ key ] = cookies[i].split('=')
+		Vue.cookie.delete(key)
+		// let eqPos = cookie.indexOf("=")
+		// let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+		// document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
+	}
+}
