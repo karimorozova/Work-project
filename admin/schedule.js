dde8ac93e7f1f4235b0d3daee82743489c923218
Maxym Palamar <maxyplmr@gmail.com>
2022-01-17 11:37:32 +0200

    Update schedule.js

--- admin/schedule.js
+++ admin/schedule.js
@@ -11,6 +11,19 @@ const { parseAndWriteLQAReport } = require('./reports/newLQAStatusFromFiles')
 const { UpdateLQAFromProject, newLQAStatusFromXTRFProjects, updateVendorBenchmarkCost } = require('./reports')
 
 
+// const { getAllSteps, addStepsToPayables } = require('./invoicingPayables')
+// const autoCreationPayablesReports = async () => {
+// 	const allSteps = await getAllSteps(0, 999999, {})
+// 	try {
+// 		await addStepsToPayables(allSteps, null)
+// 	}catch (e) {
+// 		console.log(e)
+// 	}
+// 	console.log(allSteps.length)
+// }
+// autoCreationPayablesReports()
+
+
 // downloadMemoqFile({memoqProjectId:'1443ab32-fa74-eb11-90ed-82bb18d08256', docId:'4c077bd7-e5e7-46a5-9e4e-2953ab86e913', path:'./dist/max.xlsx'} )
 // saveOtherProjectStatuses()
 // downloadFromMemoqProjectsData();
@@ -25,19 +38,19 @@ const { UpdateLQAFromProject, newLQAStatusFromXTRFProjects, updateVendorBenchmar
 // schedule.scheduleJob('30 8,13,18 * * *', async () => await scheduleJobBody(saveOtherProjectStatuses(), "Save project statuses from Gmail API"))
 // schedule.scheduleJob('40 8,13,18 * * *', async () => await scheduleJobBody(updateStatusesForOtherProjects(), "Save project statuses from Gmail API"))
 
-schedule.scheduleJob('40 0 * * *', async () => await scheduleJobBody(UpdateLQAFromProject(), "Updating LQA reports from projects data"))
-schedule.scheduleJob('30 0 * * *', async () => await scheduleJobBody(newLQAStatusFromXTRFProjects(), "Updating LQA reports from MemoqProjects data"))
-schedule.scheduleJob('30 23 * * *', async () => await scheduleJobBody(newLangReport(), "Updating lang tier data"))
+// schedule.scheduleJob('40 0 * * *', async () => await scheduleJobBody(UpdateLQAFromProject(), "Updating LQA reports from projects data"))
+// schedule.scheduleJob('30 0 * * *', async () => await scheduleJobBody(newLQAStatusFromXTRFProjects(), "Updating LQA reports from MemoqProjects data"))
+// schedule.scheduleJob('30 23 * * *', async () => await scheduleJobBody(newLangReport(), "Updating lang tier data"))
 
 // schedule.scheduleJob('10 0 * * *', async () => await scheduleJobBody(deleteEmptyOrNotCreatedByManger(), "Deleting not active vendor"));
 // schedule.scheduleJob('20 0 * * *', async () => await scheduleJobBody(sendPreventDeleteNotActiveVendor(), "Send prevent delete not active vendor"));
-schedule.scheduleJob('30 01 * * *', async () => await scheduleJobBody(updateVendorBenchmarkCost(), "Updating vendor benchmark cost"));
+// schedule.scheduleJob('30 01 * * *', async () => await scheduleJobBody(updateVendorBenchmarkCost(), "Updating vendor benchmark cost"));
 
 
-(async () => {
-	const countLQAReports = await XtrfLqa.countDocuments()
-	if (countLQAReports <= 0) parseAndWriteLQAReport()
-})()
+// (async () => {
+// 	const countLQAReports = await XtrfLqa.countDocuments()
+// 	if (countLQAReports <= 0) parseAndWriteLQAReport()
+// })()
 
 const scheduleJobBody = async (fnc, scheduleName) => {
 	console.log(
