c8f366d87c4c4c199b04538d6ca583296f2cd8f0
Maxym Palamar <maxyplmr@gmail.com>
2022-02-18 18:13:18 +0200

    Update NewLogin.vue

--- admin/src/components/NewLogin.vue
+++ admin/src/components/NewLogin.vue
@@ -1,22 +1,36 @@
 <template lang="pug">
   .login
     .login__block
-      .login__logo.center
-        img(src="../../static/email-logo2.png")
+      //.login__logo.center
+      //  img(src="../../static/email-logo2.png")
       .login__text.center
-        span.text__title Login
-        p.text__welcome Welcome back! Please login to your account.
+        .text__title Sign in
+        .text__welcome Welcome back! Please login to your account.
+
       .login__email-block
         .input__block
-          .input__title E-mail:
-          input.input__field(v-model='form.email' placeholder="example@pangea.global" )
+          .input__title E-mail
+          input.input__field(v-model='form.email' placeholder="Enter your email" )
         .input__block
           .input__title
-            span Password:
-            router-link.login__forget(to="/password-reset-request") Forget Password?
-          input.input__field(v-model='form.password' type="password" placeholder="Password")
-        input.action-button__button( type="button" value="Login" @click="login")
+            span Password
+
+          input.input__field(v-model='form.password' type="password" placeholder="Enter your password")
+
+        router-link(to="/password-reset-request")
+          .login__forget Forgot password
+
+        input.action-button__button( type="button" value="Sign in" @click="login")
+
+      .login__splitter
+        .login__splitter-text or sign in with
+
       .login__oauth
+        .icons
+          .icon
+            i(class="fa-solid fa-user")
+          .icon
+            i(class="fa-solid fa-user")
 
 </template>
 
@@ -33,13 +47,13 @@ export default {
     return {
       form: {
         email: '',
-        password: '',
+        password: ''
       }
     }
   },
   methods: {
     ...mapActions({
-      alertToggle: "alertToggle",
+      alertToggle: "alertToggle"
     }),
     async login() {
 
@@ -59,6 +73,24 @@ export default {
 
 <style scoped lang="scss">
 @import "../assets/scss/colors";
+
+.icons {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+
+.icon {
+  cursor: pointer;
+  color: $border;
+  transition: .2s ease-out;
+  font-size: 18px;
+
+  &:hover {
+    color: $dark-border
+  }
+}
+
 .login {
   background-image: url("../assets/images/signin-background.jpg");
   display: flex;
@@ -68,68 +100,96 @@ export default {
   color: $text;
   background-size: cover;
 
-  &__block {
-    background-color: $white;
-    border-radius: 4px;
-    padding: 20px 35px 35px;
-  }
+  &__splitter {
+    padding-bottom: 30px;
+    border-bottom: 1px solid $light-border;
+    margin-bottom: 30px;
+    position: relative;
 
-  &__text {
-    font-family: Myriad600;
-    margin-bottom: 10px;
-    p {
-      margin: 5px 0;
+    &-text {
+      position: absolute;
+      background-color: white;
+      top: 18px;
+      left: 110px;
+      padding: 5px;
+      color: #ccc;
     }
   }
 
-  &__logo img{
-    width: 60px;
+  &__block {
+    background-color: $white;
+    border-radius: 4px;
+    padding: 40px;
+    box-shadow: $box-shadow;
   }
 
+  &__text {
+    //margin-bottom: 10px;
+    //p {
+    //  margin: 5px 0;
+    //}
+  }
+
+  //&__logo img{
+  //  width: 60px;
+  //}
 
 
   &__forget {
     text-decoration: none;
-    color: $dark-border;
+    color: #ccc;
+    text-align: center;
   }
 
 
-  .text{
+  .text {
     &__title {
-      font-size: 25px;
+      font-size: 24px;
+      font-family: Myriad600;
     }
+
     &__welcome {
       color: $dark-border;
+      padding: 10px 0 25px;
     }
   }
+
   .input {
     &__block {
       margin-bottom: 15px;
     }
+
     &__title {
       font-size: 14px;
-      margin-bottom: 3px;
+      margin-bottom: 5px;
       display: flex;
       justify-content: space-between;
+      color: #ccc;
+      letter-spacing: 0.2px;
+
     }
+
     &__field {
       box-sizing: border-box;
-      border: none;
+      border: 1px solid $light-border;
       outline: none;
-      width: 100%;
-      height: 35px;
+      width: 320px;
+      height: 40px;
       border-radius: 4px;
-      padding: 5px 10px;
-      background-color: $light-border;
+      transition: .1s ease-out;
+      box-shadow: 0 0 0 30px white inset !important;
+      padding: 0px 10px;
+
+      &:focus {
+        border: 1px solid $border;
+      }
     }
   }
 }
 
 .action-button {
   &__button {
-    min-width: 110px;
-    padding: 0 30px 0 30px;
-    height: 35px;
+    height: 40px;
     width: 100%;
     color: $white;
     font-size: 14px;
@@ -140,7 +200,7 @@ export default {
     letter-spacing: 0.2px;
     cursor: pointer;
     transition: .05s ease-out;
-
+    margin-top: 15px;
 
     &:hover {
       filter: brightness(0.96);
@@ -154,5 +214,15 @@ export default {
 
 .center {
   text-align: center;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+  transition: .2s ease-out;
+
+  &:hover {
+    text-decoration: underline;
+  }
 }
 </style>
