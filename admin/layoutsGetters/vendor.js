a97ab3bb539c56bf270e49e2e277c444c757f1cf
PLMR <maxyplmr@gmail.com>
2022-05-12 13:23:06 +0300

    Update vendor.js

--- admin/layoutsGetters/vendor.js
+++ admin/layoutsGetters/vendor.js
@@ -24,6 +24,25 @@ module.exports = getLayoutVendors = async ({ query = {}, sort = {}, options = {}
 				fullName: { $concat: [ "$firstName", " ", "$surname" ] }
 			}
 		},
+		...(Object.keys(query).length ? [ { $match: { ...query } } ] : []),
+		{
+			$project: {
+				photo: 1,
+				currency: 1,
+				website: 1,
+				email: 1,
+				phone: 1,
+				timezone: 1,
+				native: 1,
+				gender: 1,
+				companyName: 1,
+				dateInfo: 1,
+				catExperience: 1,
+				vendorType: 1,
+				isTest: 1,
+				isAvailableForWork: 1
+			}
+		},
 		...(!!options.hasSort && Object.keys(sort).length ? [ { $sort: sort } ] : []),
 		...(!!options.hasSkip ? [ { $skip: countToSkip } ] : []),
 		...(!!options.hasLimit ? [ { $limit: countToGet } ] : [])
@@ -34,18 +53,30 @@ module.exports = getLayoutVendors = async ({ query = {}, sort = {}, options = {}
 
 
 const handlerSort = (rawSort) => {
-	const sort = {}
-	if (!Object.keys(rawSort).length) return { _id: -1 }
+	console.log('Raw Filters', rawSort)
 
+	const sort = {}
+
+	if (!Object.keys(rawSort).length) return { _id: -1 }
+	Object.keys(rawSort).forEach(el => {
+		const [ , key ] = el.split('_')
+		sort[key] = rawSort[el]
+	})
+
+	console.log('Ready Sort', sort)
 	return sort
 }
 
 const handlerQuery = (rawQuery) => {
+	console.log('Raw Filters', rawQuery)
+
 	const query = {}
 	const reg = /[.*+?^${}()|[\]\\]/g
 
 	if (rawQuery['status'] && rawQuery['status'] !== 'All') {
 		query["status"] = rawQuery['status']
 	}
+
+	console.log('Ready Filters', query)
 	return query
 }
